336995b3c3aa19095a40ada60a269295d05cbe2f
Anthony Zeitlin <zeitlin1@gmail.com>
2016-09-07 23:44:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+ return $('body p')
+}
+
+function lastImageSelector() {
+ return $('img:last')
+}
+
+function ninjaBabySelector() {
+ return $('#baby-ninja')
+}
+
+function divSelector() {
+ return $('div')
+}
+
+function firstListItem() {
+ return $('ul li:first-child')
+}
