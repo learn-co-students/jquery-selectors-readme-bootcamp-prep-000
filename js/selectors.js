521935516ab5ddbf6653a3304a377a3b72aa6e67
Cinthya Rivas <cinthyarivas28@gmail.com>
2016-09-08 00:59:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -12,9 +12,9 @@ function ninjaBabySelector(){
 }
 
 function divSelector(){
-  return $('div');
+  return $('.pics');
 }
 
 function firstListItem(){
-  return $('ul #pic-list:first-child')
+  return $('ul#pic-list li:first-child')
 }
