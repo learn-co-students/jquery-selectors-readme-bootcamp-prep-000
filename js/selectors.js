b22c4a38c3a8723721f9dc5e245aa8f89850f20b
Aaron Miller <amiller719@gmail.com>
2016-09-10 23:43:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+var paragraphSelector = function() {
+    var getPara = $('p');
+    return getPara;
+};
+
+var lastImageSelector = function() {
+    var getImg = $('img:last');
+    return getImg;
+};
+
+var ninjaBabySelector = function() {
+    var getNinja = $('#baby-ninja');
+    return getNinja;
+};
+
+var divSelector = function() {
+    var getDivs = $('.pics');
+    return getDivs;
+};
+
+var firstListItem = function() {
+    var firstLi = $('ul li:first');
+    return firstLi;
+};
