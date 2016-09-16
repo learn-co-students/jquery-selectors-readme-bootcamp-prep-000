3725dea760a47f71e093b9b09aa06ce88ca47ed0
Andres Duarte <andresduarte010@gmail.com>
2016-09-16 19:56:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('P');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('div #pic-list li:first-child')
+}
