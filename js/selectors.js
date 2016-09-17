2fbbc6c95090529d2a7452152001bbff86c91126
William Barela <william.barela.dev@gmail.com>
2016-09-17 08:00:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  myDivs = $('div');
+  return [myDivs[myDivs.length - 2], myDivs[myDivs.length - 1]]
+}
+
+function firstListItem(){
+  return $('ul li:first-child');
+}
