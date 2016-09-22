da633fc2d91f3a9941c615780fd88c11189e133b
Jonathan McAlpine <j.g.mcalpine@gmail.com>
2016-09-22 18:11:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector () {
+  var pTags = $('p');
+  return pTags;
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  var ninBabe = $('#baby-ninja');
+  return ninBabe;
+}
+
+function divSelector() {
+  var picDivs = $('.pics');
+  return picDivs;
+}
+
+function firstListItem() {
+  var firstChile = $('ul#pic-list li:first-child');
+  return firstChile;
+}
