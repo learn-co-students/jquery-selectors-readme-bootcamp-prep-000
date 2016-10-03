220866e4e1068ac33325c6dd741ccb5ee7cdb4e0
Matthew Palumbo <mmpal78@yahoo.com>
2016-10-03 17:42:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $('p');
+  return p;
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('img:first');
+}
+
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first')
+}
