d7274868080db31cc04bd792de93415ef97930ed
Chenxi Wu <wuchenxi2013@gmail.com>
2016-10-15 05:33:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,5 @@
-// declare your functions here...
+var paragraphSelector=()=>$('p')
+var lastImageSelector=()=>$('img:last')
+var ninjaBabySelector=()=>$('#baby-ninja')
+var divSelector=()=>$('.pics')
+var firstListItem=()=>$('#pic-list li:first-child')
