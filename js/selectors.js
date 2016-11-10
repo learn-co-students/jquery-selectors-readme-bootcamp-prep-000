69538ec7a69f43f2d9ad8e8a464d7a328ca94efa
Daniel Seehausen <daniel.seehausen@gmail.com>
2016-11-10 21:56:13 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+    return $('p')
+}
+
+function lastImageSelector() {
+    return $('img:last')
+}
+
+function ninjaBabySelector() {
+    return $('#baby-ninja')
+}
+
+function divSelector() {
+    return $('.pics')
+}
+
+function firstListItem() {
+    console.log($('#pic-list li:first-child'))
+    return $('#pic-list li:first-child')
+}
