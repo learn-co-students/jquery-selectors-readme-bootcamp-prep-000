3cb55efdda831ab768a230fae540c31e794919e2
Peter Choe <1peterchoe@gmail.com>
2016-12-05 06:51:09 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+var paragraphSelector = function() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
