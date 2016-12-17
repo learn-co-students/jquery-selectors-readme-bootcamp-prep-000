5b1c895782ec8b134da87332eaa5bffac8c0c9b0
Raul Vasquez <rvasqz86@gmail.com>
2016-12-17 08:37:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+function lastImageSelector() {
+  return $('img:last')
+}
+function divSelector() {
+  return $('.pics')
+}
+function firstListItem(){
+  return $('li:first-child')
+}
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
