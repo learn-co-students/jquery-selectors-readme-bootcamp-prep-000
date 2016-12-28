f0739fbf49016f11338ef9212c682ed41f3c224a
antley chu <antley.chu@gmail.com>
2016-12-28 02:09:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,31 @@
 // declare your functions here...
+$('img');
+$('.pics');
+$('#baby-ninja');
+$('ul li');
+
+
+function paragraphSelector(){
+var pArray = $('p');
+return pArray;
+};
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+};
+
+function ninjaBabySelector(){
+ var ninjaBaby = $('img#baby-ninja');
+ return ninjaBaby;
+};
+
+function divSelector(){
+ var classSelect = $('div.pics');
+ return classSelect;
+};
+
+function firstListItem(){
+var firstItem = $('ul#pic-list li:first-child');
+return firstItem;
+};
