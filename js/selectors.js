efc501baf0ec8139cd013588a3367cc17deba3ac
Kandis Tagliabue <kandistag@gmail.com>
2016-12-28 02:53:17 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector(){
+  p = $('p');
+  return p;
+}
+
+function lastImageSelector(){
+  i = $('img:last');
+  return i;
+}
+
+function ninjaBabySelector(){
+  n = $('#baby-ninja')
+  return n;
+}
+function divSelector() {
+  p = $('.pics')
+  return p;
+}
+
+function firstListItem() {
+  f = $('ul li:first-child');
+  return f;
+}
