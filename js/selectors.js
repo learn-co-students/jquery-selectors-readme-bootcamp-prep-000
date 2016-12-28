18d71048c7e18ba371f8c2ad82f247c77f6b354f
Michael Cacciano <mbcacciano@gmail.com>
2016-12-28 10:48:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  let pTags = $('p');
+  return pTags;
+}
+
+function lastImageSelector() {
+  let lastImg = $('img').last();
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  let ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector () {
+  let picsDiv = $('.pics');
+  return picsDiv;
+}
+
+function firstListItem() {
+  let firstItem = $('#pic-list li:first-child');
+  return firstItem;
+}
