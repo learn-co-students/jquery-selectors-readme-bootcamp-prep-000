2e7b727fee8a1d71e54c1426cf4c2555d5b783c9
Aditi Green <asaki444@gmail.com>
2017-01-10 20:01:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+return  $('div ul li:last-child img')
+}
+
+function ninjaBabySelector(){
+return   $("#baby-ninja")
+}
+
+function divSelector(){
+return   $('.pics')
+}
+
+function firstListItem(){
+return   $("div ul li:first-child");
+}
