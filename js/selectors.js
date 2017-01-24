ec61acdba7b9935c12919e803dafad2537c77493
Isaac Peifer <iwpeifer@gmail.com>
2017-01-24 02:43:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+paragraphSelector();
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+lastImageSelector();
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+ninjaBabySelector();
+
+function divSelector(){
+  return $('.pics');
+}
+
+divSelector();
+
+function firstListItem(){
+  return $('ul li:first-child');
+}
+
+firstListItem();
