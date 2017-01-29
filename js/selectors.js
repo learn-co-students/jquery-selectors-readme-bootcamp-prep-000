066f5e175c0fa3dd9d05a729c175d0dbf0e73341
Sara Hopper <sara.hopper@gmail.com>
2017-01-29 19:12:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var p = $('p')
+  return p
+}
+
+function lastImageSelector() {
+  var l = $('img:last')
+  return l
+}
+
+function ninjaBabySelector() {
+  var b = $('#baby-ninja')
+  return b
+}
+
+function divSelector() {
+  var c = $('.pics')
+  return c
+}
+
+function firstListItem() {
+  var f = $('ul#pic-list li:first-child')
+  return f
+}
