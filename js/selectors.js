ee53137364cf38cd471e620268b7bad5c179b75c
Brad White <white.bradleyj@gmail.com>
2017-02-02 04:36:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector(){
+  var paragraphs = $("p");
+  return paragraphs
+}
+
+function lastImageSelector(){
+  var lastImage = $("img:last");
+  return lastImage
+}
+
+function ninjaBabySelector(){
+  var ninjaBaby = $("#baby-ninja");
+  return ninjaBaby
+}
+
+function divSelector(){
+  return $(".pics")
+}
+
+function firstListItem(){
+  var fli = $("#pic-list li:first-child")
+  return fli
+}
