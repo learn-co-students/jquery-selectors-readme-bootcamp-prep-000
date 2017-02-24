83dc2dd992e68e822e38cc6db6cb0b12804e7c86
Pablo Lee <lee.pablo.e@gmail.com>
2017-02-24 07:04:34 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
-// declare your functions here...
+
+function paragraphSelector(){
+  var ptags = $('p');
+  return ptags
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
