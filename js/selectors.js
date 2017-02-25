b671a9988ee6fabb6f332c810010de7b97453115
Lawrence Weinstein <lawrence.e.weinstein@gmail.com>
2017-02-25 13:51:17 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector(){
+  return $('div').slice(0,2);
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function firstListItem(){
+  return $("ul li:first-child");
+}
