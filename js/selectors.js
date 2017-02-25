56387d03c28a93fe8eaa9ec707bff67f60a6225d
subash karki <skarki35205@hotmail.com>
2017-02-25 02:30:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,4 +1,20 @@
 // declare your functions here...
-$(document).ready(paragraphSelector(){
-    $('p')
-})
+function paragraphSelector(){
+    return $('p');
+}
+
+function lastImageSelector(){
+    return $('img:last');
+}
+
+function ninjaBabySelector(){
+    return $('#baby-ninja');
+}
+
+function divSelector(){
+    return $('.pics';)
+}
+
+function firstListItem(){
+    return $('ul#pic-list li:first-child');
+}
