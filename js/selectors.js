7489e4e7c7e1e48daa0d91f6f6c448d7ee6568a9
Rupert Saxton <rupertsaxton@gmail.com>
2017-02-28 15:05:20 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
