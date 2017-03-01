27aa7474ea32000c2bb87dfbdf70f0642e90ff75
Chirantan Ghosh <chrn.ghosh@gmail.com>
2017-03-01 02:39:14 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img').last();
+}
+
+function ninjaBabySelector() {
+  return $('img#baby-ninja');
+}
+
+function divSelector() {
+  return $('div').slice(0, 2);
+}
+
+function firstListItem() {
+  return $('ul li').first();
+}
