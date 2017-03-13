1602bc5ec40e3308d384c4abfe634a5d40270f7c
Starla P <delgado.christine@gmail.com>
2017-03-13 22:51:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+function paragraphSelector () {
+  //element selector to select p tags in index.html
+  return $('p');
+  //return p tags
+}
+
+function lastImageSelector () {
+  //use last jquery selector to return last image of index.html
+  return $('img:last');
+}
+
+
+function ninjaBabySelector () {
+  //use id selector to return ninja baby image
+  return $('#baby-ninja');
+}
+
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem () {
+  //first child selector to
+  //return first list item in ul with id pic-list
+  return $('ul#pic-list li:first-child');
+}
