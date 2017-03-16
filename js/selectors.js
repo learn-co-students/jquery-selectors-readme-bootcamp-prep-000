f0c95d37d7ec2730429b1ddb946bf65ce0da131a
Tony Distinti <adistinti@gmail.com>
2017-03-16 23:55:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  //var firsty = $('p')
+  //return firsty
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
