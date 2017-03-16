0df29aec3e12c3027d0cc00f43903eac4409434a
Abdul Rahman Quwatli <aznq2005@yahoo.com>
2017-03-16 03:13:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
 // declare your functions here...
+
+function paragraphSelector()
+{
+  var k=$('p');
+  return k;
+}
+
+function lastImageSelector()
+{
+  return $('img:last');
+}
+
+function ninjaBabySelector()
+{
+  return $('#baby-ninja');
+}
+
+function divSelector()
+{
+  return $('.pics');
+}
+
+function firstListItem()
+{
+  return $('#pic-list li:first-child');
+}
