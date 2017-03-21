9df4089515db2e8cb3ce5260f9afd94ce3501afe
Tim nguyen <timdung_tim@yahoo.com>
2017-03-21 16:26:12 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+// write a function paragraphSelector
+function paragraphSelector(){
+  var para = $('p');
+  return para;
+}
+
+function lastImageSelector() {
+var last = $('img:last')
+return last;
+}
+
+function ninjaBabySelector(){
+  var babyNinja = $('#baby-ninja')
+  return babyNinja;
+}
+
+function divSelector(){
+  var divs = $('.pics')
+  return divs;
+}
+
+function firstListItem(){
+  var firstChild = $('#pic-list li:first-child');
+  return firstChild;
+}
