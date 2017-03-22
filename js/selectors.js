b249ffb9ecdc8c5ab98da749ce898ab5ac955f12
Hou In Choi <hichoi17@outlook.com>
2017-03-22 19:50:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('div ul li:last-child img')
+  //return $('div ul li img:last')
+}
+
+function ninjaBabySelector(){
+  return $('div img#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('div ul li:first')
+}
