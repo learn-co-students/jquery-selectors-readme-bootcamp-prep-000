2d4167d36d1de20e6b61ec8f82fc28a24bf52b8f
David Squires <david.squires1@gmail.com>
2017-03-27 11:31:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $(#baby-ninja)
+}
+
+function divSelector() {
+  return $('div:2')
+}
+
+function firstListItem() {
+  return $('ul #pic-list:first-child')
+}
