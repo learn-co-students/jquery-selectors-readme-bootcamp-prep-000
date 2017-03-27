2bb8730d0668ac4f44c482cc00a38d248baa7f80
Tyler Gotto <tylergotto17@gmail.com>
2017-03-27 09:17:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+    var paragraphs =  $('p');
+    return paragraphs;
+}
+function lastImageSelector() {
+    var lastImage = $('img:last');
+    return lastImage;
+}
+function ninjaBabySelector() {
+    var ninjaImage = $('#baby-ninja');
+    return ninjaImage;
+}
+function divSelector() {
+    var picsClass = $('.pics');
+    return picsClass;
+}
+function firstListItem() {
+    var firstChild = $('ul li:first-child');
+    return firstChild;
+}
