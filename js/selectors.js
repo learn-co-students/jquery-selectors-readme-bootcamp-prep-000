e99c6ddebf8bb68a9a21637561d404b20fe4cf87
raj parsi <rajeshwar.parsi@gmail.com>
2017-03-28 14:58:24 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
 // declare your functions here...
+function paragraphSelector(){
+
+  return $('p')
+}
+
+
+function lastImageSelector(){
+  var images = $('img')
+  for ( let i=0; i< images.length ;i++){
+    if(i === images.length -1){
+      return [images[i]];
+    }
+    
+  }
+  return null;
+}
