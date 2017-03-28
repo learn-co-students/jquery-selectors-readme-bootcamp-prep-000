a5dcda0c82ae05eedad984aa1189441361d0ec47
raj parsi <rajeshwar.parsi@gmail.com>
2017-03-28 16:47:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -6,12 +6,30 @@ function paragraphSelector(){
 
 
 function lastImageSelector(){
-  var images = $('img')
-  for ( let i=0; i< images.length ;i++){
-    if(i === images.length -1){
-      return [images[i]];
-    }
+  // var images = $('img')
+  // for ( let i=0; i< images.length ;i++){
+  //   if(i === images.length -1){
+  //     return [images[i]];
+  //   }
+  //   
+  // }
+  return $("img:last")
+}
+
+
+function ninjaBabySelector(){
+
+    return $("#baby-ninja")
+}
+
+
+function divSelector(){
+    return $('.pics')
+}
+
+
+function firstListItem(){
+return  $('#pic-list li:first-child')
+  
     
-  }
-  return null;
 }
