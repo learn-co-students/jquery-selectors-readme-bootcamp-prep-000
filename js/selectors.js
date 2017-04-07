edac3ba88741bfb9c92e794e2da24000b23eeb34
Charles Wooley <cwooley89@gmail.com>
2017-04-07 17:13:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,27 @@
-// declare your functions here...
+function paragraphSelector(){
+   var pArr = $('p')
+   return pArr
+}
+
+function lastImageSelector(){
+  //var lastImage = $('html img:last-child')
+  //return lastImage
+  //
+  var lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector(){
+  var ninjaBaby = $('#baby-ninja')
+  return ninjaBaby
+}
+
+function divSelector(){
+  var picDivs = $('.pics');
+  return picDivs
+}
+
+function firstListItem(){
+  var firstLi = $('ul li:first-child')
+  return firstLi
+}
