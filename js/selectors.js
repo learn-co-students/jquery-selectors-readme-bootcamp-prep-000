0aa3d17730b7af4119442324b93380bb6d36c132
Dan M <arconk@gmail.com>
2017-04-08 13:36:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}// declare your functions here...
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child');
+}
