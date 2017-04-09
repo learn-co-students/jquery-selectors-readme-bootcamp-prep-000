12289f3d57eae1190eb0d0cc5fd8e0625518df58
Nancy Noyes <nnoyes@comcast.net>
2017-04-09 15:42:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  var a=$('.pics')
+  return a
+}
+
+function firstListItem() {
+  return $('.pics #pic-list li:first-child')
+}
