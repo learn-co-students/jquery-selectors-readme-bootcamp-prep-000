d8c79896268884aabf8ec12ed2ff81e15c5ebf9c
Oleksandr Liaskovskyi <alpa4inok@gmail.com>
2017-04-10 18:07:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('img#baby-ninja');
+}
+
+function divSelector() {
+  return $('div.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
