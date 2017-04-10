9f9377f041bf1fc03b1fd3efb6b912cef85a09ee
Miriam Hall <miriamhall.mgh@gmail.com>
2017-04-10 18:12:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraphs= $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var last=$('img:last')
+  return last
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list > li:first-child')
+}
