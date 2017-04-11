28a8d5dc87be6ddaa79f50f4475e774408990b7f
Steffen Hammelow-Berg <shammelow@gmail.com>
2017-04-11 07:32:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $(".pics ul li img:last")
+}
+
+function ninjaBabySelector(){
+  return $(".pics #baby-ninja")
+}
+
+function divSelector(){
+  return $(".pics")
+}
+
+function firstListItem(){
+  return $("#pic-list li:first-child")
+}
