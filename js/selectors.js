3d0341dc36ba308fb25dcfd835bf397b190fbe08
Krishel Lasam <krishel.lasam@gmail.com>
2017-04-15 08:21:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('img:first')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  return $('li:first')
+}
