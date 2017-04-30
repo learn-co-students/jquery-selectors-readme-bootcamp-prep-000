4b34fa7e079f70fa4ecccb71e04e7a6638402114
Talise Hendrix <spasibo09@gmail.com>
2017-04-30 18:35:11 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+var paragraphSelector= function() {
+ return $('p');
+
+}
+function lastImageSelector() {
+  return $("img:last");
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+return $(".pics");
+}
+
+var firstListItem = function() {
+  return $("#pic-list li:first-child");
+}
