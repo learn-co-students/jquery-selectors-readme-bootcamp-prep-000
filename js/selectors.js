efc2b96f9a413931d6807a4e88160d0d657958c2
Karen Berba <kberba@gmail.com>
2017-05-06 18:14:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var pTag = $('p');
+  return pTag;
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  var babyNinja = $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector() {
+  var divSelect = $('.pics');
+  return divSelect;
+}
+
+function firstListItem() {
+  var listItem_1st = $('ul#pic-list li:first');
+  return listItem_1st;
+}
