a5f39b399552dc48d6030d75dc21dbf98a9f26eb
darryl most <dmost1@gulls.salisbury.edu>
2017-05-06 11:29:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector()
+{
+  var para = $("p");
+  return para;
+}
+
+function lastImageSelector()
+{
+  return $("img:last");
+}
+
+function ninjaBabySelector()
+{
+  return $("#baby-ninja");
+}
+
+function divSelector()
+{
+  return $(".pics");
+}
+
+function firstListItem()
+{
+  return $("ul li:first-child");
+}
