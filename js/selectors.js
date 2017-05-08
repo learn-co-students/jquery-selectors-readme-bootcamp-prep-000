55f6aff075d9aef2db28a8a2a2b60926e963372a
Jonathan Penton <jonathan@penton.site>
2017-05-08 01:04:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function paragraphSelector() {
+  return $('p')
+}
