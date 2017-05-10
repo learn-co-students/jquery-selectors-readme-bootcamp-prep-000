af7373205f52a452d49ff673b11011945b08d29c
Laura Slor <slorlaura@gmail.com>
2017-05-10 00:30:54 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector () {
+var newP = $("p")
+  return newP
+}
+
+function lastImageSelector () {
+let newPic = $("img:last")
+return newPic
+}
+
+function ninjaBabySelector () {
+  const ninjaBby = $("#baby-ninja")
+  return ninjaBby
+}
+
+function divSelector () {
+  let pic = $(".pics")
+return pic
+}
+
+function firstListItem () {
+  let firstList = $("ul li:first-child")
+  return firstList
+}
