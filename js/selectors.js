eaa31efe9d00b31026b0113d1798787b14d669e2
Ginger Wilson <connor.wilson221@gmail.com>
2017-05-10 01:27:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+//This is a jQuery selector function, its function is to pick the paragraph tag in the html. Then it returns it.
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('ul li:last-child img')
+}
+
+function ninjaBabySelector(){
+  return $('div #baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
