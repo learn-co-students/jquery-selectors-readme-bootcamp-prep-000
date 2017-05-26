4c616e9b46d2cc8ce3ac562b3f407a26a1e94eb8
william hancharek <william.hancharek@gmail.com>
2017-05-26 01:36:40 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+$('.pics')
+
+$('#baby-ninja')
+
+$('ul li')
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child')
+}
