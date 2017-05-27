746d717684fca1fadd2912b0969e40c1dc192f12
Vivian Kulumba <vkulumba@gmail.com>
2017-05-27 02:40:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+var paragraphSelector = function() {
+  return $('p')
+}
+
+var lastImageSelector = function() {
+  return $('img:last')
+}
+
+var ninjaBabySelector = function() {
+  return $('#baby-ninja')
+}
+
+var divSelector = function() {
+  return $('.pics')
+}
+
+var firstListItem = function() {
+  return $('ul#pic-list li:first-child')
+}
