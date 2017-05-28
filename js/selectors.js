3e16e99235e52a329b4c8d585dca651a8fb2999f
Tina Su <tinasu74@gmail.com>
2017-05-28 20:17:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+// select the p tags in index.html and return those elements
+function paragraphSelector() {
+  return $('p');
+}
+
+// return the last image in index.html
+function lastImageSelector() {
+  return $('img:last');
+}
+
+// use an ID selector to return the ninja baby image
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+// use a class selector to return the two divs with the class pics
+function divSelector() {
+  return $('.pics')
+}
+
+// return the first list item in the ul with the ID pic-list
+function firstListItem() {
+  return $('div ul li:first-child')
+}
