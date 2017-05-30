ce6f974978414cf332664a3fef7816caab6401ba
Robert Laws <roblaws@me.com>
2017-05-30 12:55:46 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last')
+  return lastImg
+}
+
+function ninjaBabySelector() {
+  var ninja = $('#baby-ninja')
+  return ninja
+}
+
+function divSelector() {
+  var picDivs = $('div.pics')
+  return picDivs
+}
+
+function firstListItem() {
+  var firstLI = $('ul#pic-list li:first-child')
+  return firstLI
+}
