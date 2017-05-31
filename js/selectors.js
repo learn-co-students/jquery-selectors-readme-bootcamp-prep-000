9cfe20fceaf82f556f763e6b046f0c4cc59ecf95
Ben Folk-Sullivan <benfolksullivan@gmail.com>
2017-05-31 16:23:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
 // declare your functions here...
+function paragraphSelector(){
+  var para = $('p')
+  return para
+}
+
+function lastImageSelector(){
+  var img = $('img:last')
+  return img
+}
+
+function ninjaBabySelector(){
+  var baby = $('#baby-ninja')
+  return baby
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
