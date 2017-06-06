edd1f596fa35377631be0b676ee1de59c3f0fd08
Elijah Lewis <elewis9989@gmail.com>
2017-06-06 03:36:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('div img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  var array = ['https://s3.amazonaws.com/after-school-assets/awesome.jpg'];
+  return array;
+}
