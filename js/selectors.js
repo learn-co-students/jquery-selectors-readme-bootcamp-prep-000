c9349ff6363b677f28ca2b96fe20887690277da2
Taurean Branch <tdbranchii@gmail.com>
2017-06-07 19:38:38 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  var p = $('p')
+  return p
+}//end paragraphSelector
+function lastImageSelector(){
+  var last = $('img:last')
+  return last
+}//end lastImageSelector
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja');
+  return ninja
+}//end ninjaBabySelector
+function divSelector(){
+  var pics = $('.pics');
+  return pics
+}//divSelector
+function firstListItem(){
+  var first = $('#pic-list li:first-child')
+  return first
+}
