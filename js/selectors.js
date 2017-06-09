4cf916b30ea5ac4fa83df61913a1dc24f829ea02
Andrew James <ajames20@gmail.com>
2017-06-09 15:02:00 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector(params) {
+  return $('img').last();
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('li').slice(0, 1);
+}
