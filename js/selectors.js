fb2bb2b2fcc0ff286a00f38e7611a76884b5e239
Sam Crowder <samecrowder@gmail.com>
2017-06-12 20:35:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+ return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function getPicList(){
+  return $('#pic-list');
+}
+
+function firstListItem(){
+  console.log($('#pic-list :first-child'))
+  return $('#pic-list li:first-child')
+}
