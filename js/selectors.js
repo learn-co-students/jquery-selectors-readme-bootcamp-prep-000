a63b9976ab2870b8f4b19ec72fbc8cd74d0974a8
Michael Anderson <michanders@msn.com>
2017-06-13 19:27:48 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  var pSelect = $('p');
+  return pSelect;
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector(){
+  var ninja = $("#baby-ninja");
+  return ninja;
+}
+
+function divSelector(){
+  var div = $("div.pics");
+  return div;
+}
+
+function firstListItem(){
+  var first = $("#pic-list li:first-child");
+  return first;
+}
