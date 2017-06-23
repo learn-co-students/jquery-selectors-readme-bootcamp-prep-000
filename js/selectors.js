094fc455d46028ac880781d627a70e9cdf56c17f
Charlie Rosen <dherzl@protonmail.com>
2017-06-23 20:22:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
