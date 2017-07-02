2ee90568374f5ab3a6a6ce06f762ecb18ef814f5
Bastian Bernst <bastian.bernst@googlemail.com>
2017-07-02 15:23:39 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  let paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  let lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  let ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  let divs = $('.pics');
+  return divs;
+}
+
+function firstListItem() {
+  let first = $("#pic-list li:first-child");
+  return first;
+}
