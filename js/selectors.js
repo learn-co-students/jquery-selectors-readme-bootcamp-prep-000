b7f99cfaca3953804242767bd3256f504077403a
Daniel Toma <danieltoma@ubalumni.com>
2017-07-04 00:52:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,10 @@
 // declare your functions here...
+function paragraphSelector() {
+  pArray = $('p')
+  return pArray
+}
+
+function lastImageSelector() {
+  lastImage = $('img:last')
+  return lastImage
+}
