6f190b9e248a9e9d2d83f18b12d41f6f1c6754e7
Jonathan Magolnick <jonathanmagolnick@gmail.com>
2017-07-05 17:56:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  var x = $('p')
+  return x
+}
+
+function lastImageSelector(){
+  var x = $('img:last')
+  return x
+}
+
+function ninjaBabySelector(){
+  var x = $("img[id='baby-ninja']")
+  return x
+}
+
+function divSelector(){
+  var x = $('.pics')
+  return x
+}
+
+function firstListItem(){
+  var x = $('ul li:first-child')
+  return x
+}
