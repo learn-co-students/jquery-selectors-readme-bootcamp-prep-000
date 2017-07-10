6a8c7a3696b52b967cf7856ecb30879b438242f7
Nicky Liu <nickyliu91@gmail.com>
2017-07-10 08:46:32 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var image = $('img:last')
+  return image
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first-child')
+}
