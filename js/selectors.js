40fe3f54fa93f876962f96cb2530f0fc1fd3deaa
Christina Pan <christina.pl.pan@gmail.com>
2017-07-19 00:50:04 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,20 +1,19 @@
-// declare your functions here...
 function paragraphSelector() {
   return $('p');
-};
+}
 
 function lastImageSelector() {
   return $('img:last');
-};
+}
 
 function ninjaBabySelector() {
   return $('#baby-ninja');
-};
+}
 
 function divSelector() {
   return $('.pics');
-};
+}
 
 function firstListItem() {
-  return $('ul#pic-list li:first-child');
-};
+  return $('#pic-list li:first-child')
+}
