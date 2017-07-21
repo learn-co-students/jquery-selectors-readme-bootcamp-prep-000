93919acb740e7606ac57ba59c08866a27630076a
John Eckert <johnteckert@gmail.com>
2017-07-21 20:05:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,37 @@
 // declare your functions here...
+
+//1
+
+
+function paragraphSelector() {
+  var paragraph = $('p');
+  return paragraph;
+}
+
+//2
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+//3
+
+function ninjaBabySelector() {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+//4
+
+function divSelector() {
+  var picsDiv = $('.pics');
+  return picsDiv;
+}
+
+//5
+
+function firstListItem() {
+  var firstItem = $('#pic-list li:first-child');
+  return firstItem;
+}
