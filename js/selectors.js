a96de2f82a6453e47630f5f376526c482ebeec06
Eric Lau <cireual510@gmail.com>
2017-07-22 09:05:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return document.body.querySelectorAll('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')//document.body.querySelectorAll('.pics')
+}
+
+function firstListItem() {
+  return $("#pic-list li:first-child")//('ul li:first-child')
+}
