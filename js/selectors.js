dad2240f2d08aab4ba7926ed92f0ae35b11cfe84
Filip Pietroń <filip.pietron@gmail.com>
2017-07-26 11:55:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  $('.pics')
+}
+
+function divSelector(){
+  return $('ul[id=pic-list] li:first-child')
+}
+function firstListItem(){
+  return $('ul li:first-child')
+}
