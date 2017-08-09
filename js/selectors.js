d41bc6f00f13c7c6b2938294c0b71e7c543f8f18
Malachi Price <malachiprice1@gmail.com>
2017-08-09 16:09:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector () {
+  var selection = $('p');
+  return selection;
+}
+
+function lastImageSelector () {
+  var selection = $('img:last');
+  return selection;
+}
+
+function ninjaBabySelector() {
+  var selection = $('#baby-ninja');
+  return selection;
+}
+
+function divSelector() {
+  var selection = $('.pics');
+  return selection;
+}
+
+function firstListItem() {
+  var selection = $('ul li:first-child');
+  return selection;
+}
