28d0e78bb67ee0d61b16cabeacce189918a02f67
hailey endres <hailey.endres@gmail.com>
2017-08-12 17:58:03 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
-// declare your functions here...
+function paragraphSelector() {
+  var paragraphs= []
+  paragraphs= $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var lastPic= $('img:last')
+  return lastPic
+}
+
+function ninjaBabySelector() {
+  ninjaBabes= $('#baby-ninja')
+  return ninjaBabes
+}
+
+function divSelector() {
+  divsForDays= $('.pics')
+  return divsForDays
+}
+
+function firstListItem() {
+  firstItem= $('ul li:first-child')
+  return firstItem
+}
