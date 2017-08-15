be210fab37eb5f2b3bbe393f94524b063271fe96
George gore9213@gmail.com <georgelgore@gmail.com>
2017-08-15 23:37:32 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $("p");
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $("div:lt(2)");
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child');
+}
