04a1e73deddc3578ad1821fa07fe5153373fea8b
Robin Pahlman <robin.pahlman@gmail.com>
2017-08-15 22:09:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraph = $('p');
+  return paragraph;
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector() {
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector() {
+  var divSel = $('.pics');
+  return divSel;
+}
+
+function firstListItem() {
+  var first = $('#pic-list li:first-child');
+  return first;
+}
