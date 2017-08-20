d1104c89874002a9f14dbba7ddde8225327cea98
Vanessa George <vanessa.s.george@gmail.com>
2017-08-20 01:38:57 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -3,3 +3,21 @@
 function paragraphSelector(){
   return $('p')
 }
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
