62d45f491d7b47c9193438d234618a3c6952302c
omikolaj <oscar222@sbcglobal.net>
2017-08-20 14:48:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  var elements = $('p')
+  return elements
+}
+function lastImageSelector(){
+  var elements = $('img:last')
+  return elements
+}
+function ninjaBabySelector() {
+  var elements = $('#baby-ninja')
+  return elements
+}
+function divSelector(){
+  var elements = $('.pics')
+  return elements
+}
+function firstListItem(){
+  var elements = $('ul li:first-child')
+  return elements
+}
