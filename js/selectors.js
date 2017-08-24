ca6d25c0a483aa187ee5c17ae63e64771677ce3d
John Day <itisjohnday@gmail.com>
2017-08-24 16:37:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -27,3 +27,29 @@ function firstListItem() {
   let first = $('.pics #pic-list li:first-child')
 	return first
 }
+
+/*
+SOULTIONS
+
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
+
+*/
