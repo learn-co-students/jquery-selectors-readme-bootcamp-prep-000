298658f1c32e4bbea8f9f4c17aa2d4c5bcb59417
John Day <itisjohnday@gmail.com>
2017-08-24 16:35:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 // declare your functions here...
+function paragraphSelector() {
+	  let paragraphs = $('p');
+	  return paragraphs
+	}
+
+function lastImageSelector() {
+	  let lastImage = $('img:last')
+	  return lastImage;
+}
+
+
+function ninjaBabySelector() {
+	  let ninjaBaby = $('#baby-ninja')
+	  return ninjaBaby
+}
+
+
+function divSelector() {
+  let pics = $('.pics')
+  return pics
+
+}
+
+
+function firstListItem() {
+  let first = $('.pics #pic-list li:first-child')
+	return first
+}
