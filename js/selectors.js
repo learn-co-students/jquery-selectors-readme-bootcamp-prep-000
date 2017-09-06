be0c385190d1325e29a2501f5c50ca85cc3c38f9
Julie Singh <jsingh@alumni.iu.edu>
2017-09-06 19:15:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  var element = $('p')
+  return element
+}
+
+function lastImageSelector(){
+  var element = $('img:last')
+  return element
+}
+
+function ninjaBabySelector(){
+  var element = $('#baby-ninja')
+  return element
+}
+
+function divSelector(){
+  var element = $('div.pics')
+  return element
+}
+
+function firstListItem(){
+  var element = $('ul#pic-list li:first-child')
+  return element
+}
