322b8f26b9ce5630a2f5eb71b6cfc237af1ec5bf
Antonio Reid <read.musik@gmail.com>
2017-09-08 02:40:14 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var elmSlct = $('p')
+  return elmSlct
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last')
+    return lastImg
+}
+
+function ninjaBabySelector() {
+  var babyNinja = $('#baby-ninja')
+    return babyNinja
+}
+
+function divSelector() {
+  var divPics = $('.pics')
+    return divPics
+}
+
+function firstListItem() {
+  var firChld = $('#pic-list li:first-child')
+    return firChld
+}
