fc5de0e8114f4165d99a745cce30c20251a37ab8
Melwyn <melwyn@mimpix.co.uk>
2017-09-16 12:03:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  let paragraph = $('p')
+  return paragraph
+}
+
+function lastImageSelector() {
+  let lastImg = $('img:last')
+  return lastImg
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child')
+}
