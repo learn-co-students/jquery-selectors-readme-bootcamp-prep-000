e59e52a55174a9ceb8f8ed80ae5499cb312dcffe
Kayla Hansen <shiverr@gmail.com>
2017-09-18 00:41:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $('p');
+  return p;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul li:first-child');
+}
