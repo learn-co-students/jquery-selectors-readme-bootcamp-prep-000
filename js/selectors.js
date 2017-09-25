9c0b7e7a9a8a6c03024a379d9f020370ad59a011
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-25 20:26:27 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,42 @@
 // declare your functions here...
+
+function paragraphSelector() {
+
+  var words = $('p');
+
+  return words;
+
+}
+
+
+function lastImageSelector() {
+
+  var imgSel = $('div img:last');
+
+  return imgSel;
+
+}
+
+function ninjaBabySelector() {
+
+  var ninBaby = $('#baby-ninja');
+
+  return ninBaby;
+
+}
+
+function divSelector() {
+
+  var pic = $('.pics');
+
+  return pic;
+
+}
+
+function firstListItem() {
+
+  var firstChild = $('div ul li:first-child');
+
+  return firstChild;
+
+}
