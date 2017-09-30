850198d3a32732e414aadc3872cc13c1fa2e3936
Tyrone Scafe <tscafejr@gmail.com>
2017-09-30 02:43:41 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+var paragraphSelector = function(){
+  var a = $('p');
+  return a;
+}
+
+var lastImageSelector = function(){
+  var b = $('img:last');
+  return b;
+}
+
+var ninjaBabySelector = function(){
+  var c = $('#baby-ninja');
+  return c;
+}
+
+var divSelector = function(){
+  var d = $('.pics');
+  return d;
+}
+
+var firstListItem = function(){
+  var e = $('ul li:first-child')
+  return e;
+}
