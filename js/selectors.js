12f159b411a3e249d7fedda3c0cece749fd0d9f3
Adam Clayman <adam@clayman.org>
2017-09-30 15:19:15 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+// $(document).ready(function () {
+function paragraphSelector() {
+  const paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  const lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  const babyNinja = $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector() {
+  const divsWithClassPic = $("div[class='pics']");
+  return divsWithClassPic;
+}
+
+function firstListItem() {
+  const firstChildOfListItem = $("#pic-list li:first-child");
+  return firstChildOfListItem;
+}
+
+// });
