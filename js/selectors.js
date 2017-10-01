3558dc298513b868c0ddce1fa7bce8f4e012c8f9
Nolan Byrnes <nolanehs@gmail.com>
2017-10-01 02:19:44 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+
+  return $('#pic-list li:first-child');
+}
