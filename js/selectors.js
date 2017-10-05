ad7e4352eeca73db1ccd1efe5f717c22b38a1172
Cristy Lucke <cristy.lucke@gmail.com>
2017-10-05 16:58:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,24 +1,24 @@
 // Write a function paragraphSelector that does not accept any parameters. The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
 function paragraphSelector() {
-  $('p')
+  return $('p')
 }
 
 // Write a function lastImageSelector that does not accept any parameters. The function should use the last jQuery selector to return the last image in index.html.
 function lastImageSelector() {
-  $('img:last')
+  return $('img:last')
 }
 
 // Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
 function ninjaBabySelector() {
-  $('#baby-ninja')
+  return $('#baby-ninja')
 }
 
 // Write a function divSelector that does not accept any parameters. The function should use a class selector to return the two divs with the class pics.
 function divSelector() {
-  $('div .pics')
+  return $('div.pics')
 }
 
 // Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list.
 function firstListItem() {
-  $('ul li #pic-list:first-child')
+  return $('ul#pic-list li:first-child')
 }
