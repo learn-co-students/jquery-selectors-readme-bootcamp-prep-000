3ee3c9fcf4f4abe49d6bbcd156ad9fe6e9af0038
Shibani Mookerjee <shibani.mookerjee@gmail.com>
2017-10-09 17:50:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let tags = $('p');
+  return tags;
+}
+
+function lastImageSelector(){
+  let image = $('img:last');
+  return image;
+}
+
+function ninjaBabySelector(){
+  let image = $('#baby-ninja');
+  return image;
+}
+
+function divSelector(){
+  let pics = $('.pics');
+  return pics;
+}
+
+function firstListItem(){
+  let first = $('ul#pic-list li:first-child');
+  return first;
+}
