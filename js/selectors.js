78bee6ad794d05de53b30661b17894deb63ed8cf
Katy Carr <khcarr9@gmail.com>
2017-10-26 17:16:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  let paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
