c27b15eb6af8463a5f9194bfc0c12130784ff530
Ramesh <gurams@gmail.com>
2017-10-30 01:51:54 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+
+function paragraphSelector(){
+//const abc=[];
+var abc =  $('p')
+console.log("test" + abc);
+return abc
+}
+function lastImageSelector(){
+  var lastElement = $('img:last')
+  return lastElement
+}
+function ninjaBabySelector(){
+  var lastElement = $('#baby-ninja')
+  return lastElement
+}
+function divSelector(){
+  var lastElement = $('.pics')
+  return lastElement
+}
+function firstListItem(){
+  var frstLstItm = $('li:first-child()')
+  return frstLstItm
+}
