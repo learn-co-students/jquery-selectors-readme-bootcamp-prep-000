9bd5d3b68254f7cde9bd5b408d78d65bf15cabdd
Charlie Sparks <csparks@antioch.edu>
2017-11-07 20:08:41 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,23 @@
-// declare your functions here...
+function paragraphSelector(){
+  var select = $("p");
+  return select;
+}
+
+function lastImageSelector(){
+  var select = $("img:last");
+  return select;
+}
+
+function ninjaBabySelector(){
+  var select = $("#baby-ninja");
+  return select;
+}
+
+function divSelector(){
+  var select = $(".pics");
+  return select;
+}
+
+function firstListItem(){
+  return $("#pic-list li:first-child");
+}
