5e05b8ecc7bdf664ed9f573f51b82e473409695e
yasser mimouni <yasmimouni2@gmail.com>
2017-11-17 16:27:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
-// declare your functions here...
+
+function paragraphSelector()
+{
+  return document.querySelectorAll("p");
+}
+
+function lastImageSelector()
+{
+  let images = $("img");
+  return images.slice(images.length - 1);
+}
+
+function ninjaBabySelector()
+{
+  let babyNinja = $("img#baby-ninja");
+  return babyNinja;
+}
+
+function divSelector()
+{
+  let divs = $("div");
+  return divs.slice(0, 2);
+}
+
+function firstListItem()
+{
+  let list = $("ul");
+  let arr = [list[0].firstElementChild];
+  return arr;
+}
