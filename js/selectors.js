2fd78c3ef511d09e41acea09e6aa33225d93ab64
Anthony Bracewell <anthonybracewell.violinist@gmail.com>
2017-11-28 15:11:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 // declare your functions here...
+function paragraphSelector(){
+  return  $('p')
+}
+
+function lastImageSelector(){
+  return $("img:last")
+}
+function ninjaBabySelector(){
+  return $("#baby-ninja")
+}
+function divSelector(){
+  return $(".pics")
+}
+function firstListItem(){
+  $('parent-tag child-tag:first-child')
+  return $("ul li:first-child")
+}
