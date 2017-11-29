225f25aa42416cb3dc81332ac00f0ffbd0bbaf65
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-29 21:37:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  var pSelected = $('p');
+  return pSelected;
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector(){
+  var babyNinja = $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector(){
+  var divs = $('.pics');
+  return divs;
+}
+
+function firstListItem(){
+  var firstItem = $('#pic-list li:first-child');
+  return firstItem;
+}
