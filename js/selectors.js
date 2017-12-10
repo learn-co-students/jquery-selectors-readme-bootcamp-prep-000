7c237919a41d0f2af67034903683e1e035da7f01
Brian <deltoro.brian@gmail.com>
2017-12-10 19:30:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  var para = $('p');
+  return para;
+}
+
+function lastImageSelector(){
+  var images = $('img:last');
+  return images;
+}
+
+function ninjaBabySelector(){
+  var NB = $('#baby-ninja')
+  return NB;
+}
+
+function divSelector(){
+  var divs = $('.pics');
+  return divs;
+}
+
+function firstListItem(){
+  var FLI = $('ul li:first-child');
+  return FLI;
+}
