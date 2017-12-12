25c07736faaaaffe9e9416feaff436d53a8d4bf2
Rani <rani76@gmail.com>
2017-12-12 10:49:18 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,15 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+function lastImageSelector() {
+  return $('img:last')
+}
+function ninjaBabySelector() {
+  return $("img[id='baby-ninja']")
+}
+function divSelector() {
+  return $('.pics')
+}
+function firstListItem() {
+return $("ul#pic-list li:first-child")
+}
