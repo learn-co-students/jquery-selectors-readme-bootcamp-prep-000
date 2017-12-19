4d0d55b7d9f3f977fc9ea979c593cb656a5017fd
Jay G <playerjayho@gmail.com>
2017-12-19 19:24:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector() {
+  var git = $('p');
+  return git;
+}
+
+function lastImageSelector(){
+  var tut = $('div img:last-child').last();
+  return tut;
+}
+
+function divSelector(){
+  var gu = $('.pics');
+  return gu;
+}
+
+function firstListItem(){
+  var guh = $('ul li:first-child');
+  return guh;
+}
+
+
+function ninjaBabySelector(){
+  var guh = $('#baby-ninja');
+  return guh;
+}
