d40459bdd53e831c03f0aaff16d60aed6430fcaa
Beth Schofield <bethmschofield@gmail.com>
2017-12-20 23:59:03 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector(){
+  var p = $('p');
+  return p;
+}
+
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector(){
+  var picsDivs = $('div.pics');
+  return picsDivs;
+}
+
+function firstListItem(){
+  var first = $('ul#pic-list li:first-child');
+  return first;
+}
