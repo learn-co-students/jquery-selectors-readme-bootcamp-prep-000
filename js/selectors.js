90cb9a6f148c52cb366f3882e9932ab86e37f94d
Salma Elshahawy <salmaeng71@gmail.com>
2017-12-26 10:38:00 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+  function paragraphSelector() {
+    return $("p");
+  }
+  paragraphSelector();
+
+  function lastImageSelector() {
+    return $("img:last");
+  }
+
+  function ninjaBabySelector() {
+    return $("#baby-ninja");
+  }
+
+  function divSelector() {
+    return $(".pics");
+  }
+
+  function firstListItem() {
+    return $("ul#pic-list li:first-child");
+  }
