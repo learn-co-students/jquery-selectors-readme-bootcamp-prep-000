f953ea9d3e579d2037b3ebf39c8c6862ca470e75
Ryan Inawat <rrinawat@gmail.com>
2017-12-27 23:54:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+//selects paragraph
+function paragraphSelector(){
+return $(`p`)
+}
+//selects last image in whole .hmtl document
+function lastImageSelector(){
+return  $('img:last')
+}
+//selets baby ninja
+function ninjaBabySelector(){
+return  $('#baby-ninja')
+}
+// uses class selector to return dibs with the class .pics
+function divSelector(){
+return  $('.pics')
+}
+//use first-child selector to return first list item in the ul wihtthe ID pic-list
+function firstListItem(){
+return  $('ul li:first-child')
+}
