dbfff8208bd4442e69286dd99438f7c400068b9b
Mohamad Khayat <moek91@outlook.com>
2017-12-29 15:10:53 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -8,11 +8,9 @@ function lastImageSelector(){
 function ninjaBabySelector(){
   return $('#baby-ninja')
 }
-//class selector to return the two divs with the class pics
 function divSelector(){
   return $('div.pics')
 }
-//use a first-child selector to return the first list item in the ul with the ID pic-list
 function firstListItem(){
   return $('ul#pic-list li:first-child')
 }
