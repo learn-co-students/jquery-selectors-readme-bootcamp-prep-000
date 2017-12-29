a2c259bfa933df4aabbf140cbaf335e1a9ab0c63
Mohamad Khayat <moek91@outlook.com>
2017-12-29 15:08:54 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+//class selector to return the two divs with the class pics
+function divSelector(){
+  return $('div.pics')
+}
+//use a first-child selector to return the first list item in the ul with the ID pic-list
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
