6588d1bf628d7641bf99c588beb446a5f289b67d
Peter Coughlin <coughlin1966@gmail.com>
2018-01-03 16:09:32 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,15 @@
-// declare your functions here...
+/*
+$('img')
+$('.pics')
+$('#baby-ninja')
+$('ul li')
+$('div img:first-child')
+$("img[alt='the beatles making faces']")
+$('div:last')
+*/
+
+function paragraphSelector() {
+var p = []
+p = $('p')
+  return p
+}
