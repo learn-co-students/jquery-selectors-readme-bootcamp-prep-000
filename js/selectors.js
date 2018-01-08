f1bbf38f4f4ca1f240301b725adf0a0b7d29f424
Ryan Barley <rmbarley@gmail.com>
2018-01-08 18:06:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+var paragraphSelector = () => {
+  return $('p');
+}
+
+var lastImageSelector = () => {
+  return $('img:last');
+}
+
+var ninjaBabySelector = () => {
+  return $('#baby-ninja');
+}
+
+var divSelector = () => {
+  return $('div.pics');
+}
+
+var firstListItem = () => {
+  return $('li:first-child');
+}
