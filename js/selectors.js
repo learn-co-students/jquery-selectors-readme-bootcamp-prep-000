2d52c9f5571a8a9154e8a8ecb8433c34c2fdf3f7
kendocode <git@kendocoding.com>
2018-01-17 21:13:53 -0600

    Select the selectors

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  $('p')
+}
+
+function lastImageSelector() {
+  $('img:last')
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+}
+
+function firstListItem() {
+  $('#pic-list li:first-child')
+}
+
+
+
