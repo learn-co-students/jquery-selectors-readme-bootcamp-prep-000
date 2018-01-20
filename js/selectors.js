003287f0393b9f7f946c32394c86703b7c54dae4
Laurents Mohr <laurents.mohr@whu.edu>
2018-01-20 19:29:11 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  var pTags = $('p');
+  return pTags;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
