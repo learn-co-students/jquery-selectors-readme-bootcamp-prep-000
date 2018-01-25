c7d9257a669301f7f67758cd11e9404c34be2e82
Spencer Whitehead <spencerwhitehead7@gmail.com>
2018-01-25 20:57:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector(){
+  let images = $('img:last')
+  return images
+}
+
+function ninjaBabySelector(){
+  let ninjaBaby = $('#baby-ninja')
+  return ninjaBaby
+}
+
+function divSelector(){
+  let pics = $('.pics')
+  return pics
+}
+
+function firstListItem(){
+  let item = $('ul li:first-child')
+  return item
+}
