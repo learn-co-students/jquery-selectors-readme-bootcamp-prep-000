91c7b49857f73fed00d33454b4fb9dbef15a5816
Daniel Brazel <dbrazel4@gmail.com>
2018-02-02 17:50:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,28 @@
 // declare your functions here...
+$('.pics');
+$('#baby-ninja');
+$('ul li');
+$('div img:first-child');
+$("img[alt='the beatles making faces']");
+$('div:last');
+
+function paragraphSelector(){
+  paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
