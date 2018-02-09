52ce438557629e56ddb472cc1e54236c2a0f18fa
Rosie Wang <rosiedwang@yahoo.com>
2018-02-09 21:37:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+// declare your functions here..
+
+function paragraphSelector()
+{
+   return $('p');
+}
+function lastImageSelector()
+{
+  var list =$('img:last');
+  return list;
+}
+function ninjaBabySelector()
+{
+  return $('#baby-ninja');
+}
+function divSelector()
+{
+  return $('.pics');
+}
+function firstListItem()
+{
+  var list = $('ul li:first-child');
+  return list
+}
