e4feeebb7bf524578a4a4c6e9071a80338cf8543
Santosh SN <nasturtus@gmail.com>
2018-02-10 02:28:41 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+   return $('.pics #pic-list li:last-child img')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('.pics #pic-list li:first-child')
+}
