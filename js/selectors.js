191f96ef7d5a013fbe3b965401f47f940ac62800
Trevor Seitz <trevinla@gmail.com>
2018-02-21 21:25:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
-// declare your functions here...
+function paragraphSelector() {
+  var list = $('p');
+
+  return list;
+}
+
+function lastImageSelector() {
+  var last = $('img:last');
+
+  return last
+}
+
+function ninjaBabySelector() {
+  var baby = $('#baby-ninja');
+
+  return baby;
+}
+
+function divSelector() {
+  var picDiv = $('.pics');
+
+  return picDiv;
+}
+
+function firstListItem() {
+  // var firstKid = $('ul:first-child.#pic-list');
+  var firstKid = $("ul li:first-child"); 
+  return firstKid;
+}
