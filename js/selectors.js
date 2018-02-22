e3ddfc068af24733bd29d5516b851e279e917605
T Law <redirect@gmail.com>
2018-02-22 22:27:36 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector () {
+  var p = $('p');
+  return p;
+}
+
+function lastImageSelector () {
+  var lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector () {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector () {
+  var picDiv = $('.pics');
+  return picDiv;
+}
+
+function firstListItem () {
+  var firstListItem = $('#pic-list li:first-child');
+  return firstListItem;
+}
