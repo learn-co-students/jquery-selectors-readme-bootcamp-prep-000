229955cf33ac0e640c5a8bc087d29ecd7a74346b
Matt Zuzolo <mattzuzolo@outlook.com>
2018-02-22 01:53:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,31 @@
 // declare your functions here...
+
+function paragraphSelector () {
+  
+  return $('p');
+}
+
+function lastImageSelector() {
+  
+  //just "img.last" — dont need any . or #. Because it's searching through ALL images for the first. No need to reduce scope.
+  
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+
+  //# before baby-ninja because it works the same was asa a class selector
+  return $('#baby-ninja');  
+}
+
+function divSelector(){
+  
+  //period before pics because it'll return entire class
+  return $('.pics');
+}
+
+function firstListItem(){
+  
+  //descendent selector + first child selector (see lesson for)
+  return $('ul li:first-child');
+}
