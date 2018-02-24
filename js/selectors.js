a3069b7de7c51279d036ef57ce9ca055b679e628
Luke Conley <luke.sovern.conley@gmail.com>
2018-02-24 18:10:01 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector(){
+  let text = $('p');
+  return text;
+}
+
+function lastImageSelector(){
+  let finalPic = $('img:last');
+  return finalPic;
+}
+
+function ninjaBabySelector() {
+  let babyPic = $('#baby-ninja');
+  return babyPic;
+}
+
+function divSelector(){
+  let picsDivs = $('.pics');
+  return picsDivs;
+}
+
+function firstListItem(){
+  $('parent-tag child-tag:first-child');
+  let listItem = $('#pic-list li:first-child');
+  return listItem;
+}
