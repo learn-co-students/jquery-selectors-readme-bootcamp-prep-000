b76a2221df02d07cf7fab82c54b51acca8b1e957
Charles Comer <charles.comer68@gmail.com>
2018-03-10 07:47:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  var p = $('p');
+  return p;
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector(){
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector(){
+  var divPic = $('.pics');
+  return divPic;
+}  
+function firstListItem(){
+  var firstItem = $('#pic-list li:first-child ');
+  return firstItem;
+}
+
