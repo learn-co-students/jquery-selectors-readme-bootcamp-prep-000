9cc18802e6e3eb1c60c2dd737a76128e345beb05
ksha1018@naver.com <ksha1018@naver.com>
2018-03-11 13:44:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector(){
+  const ptaglist = $("p");
+  return ptaglist;
+}
+
+function lastImageSelector(){
+  const lastimage = $("img:last");
+  return lastimage;
+}
+
+function ninjaBabySelector(){
+  const ninjababy = $("#baby-ninja");
+  return ninjababy;
+}
+
+function divSelector(){
+  const divpics = $(".pics");
+  return divpics;
+}
+
+function firstListItem(){
+  const firstlist = $("ul li:first-child");
+  return firstlist;
+}
+
