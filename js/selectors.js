bcb91dfa5dfc7e0c0772ee9461edf24086967dee
Shirley Collins <shirley.ednave@gmail.com>
2018-03-14 23:29:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var pSelector = $('p');
+  return pSelector;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  var picSelector = $('.pics');
+  return picSelector;
+}
+
+function firstListItem() {
+  var firstChild = $('ul li:first-child');
+  return firstChild;
+}
