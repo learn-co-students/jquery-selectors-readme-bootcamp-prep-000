12a1278ab325c6a1803033e0d01781513c839353
Sreepriya Srijit <sreepriyakurup87@gmail.com>
2018-03-19 13:24:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector(){
+ return  $('p');
+}
+
+function lastImageSelector()
+{
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return &('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem()
+{
+  return $('ul first-child:#pic_list')
+}
