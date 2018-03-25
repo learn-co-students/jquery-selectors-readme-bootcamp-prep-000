67ecf803acfe70857178d0a58cfb61215e5c1067
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-25 04:41:41 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,37 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  $(document).ready(function() {
+    output = $('p');
+  });
+  return output;
+}
+
+function lastImageSelector() {
+  $(document).ready(function() {
+    output = $('img:last');
+  });
+  return output;
+}
+
+function ninjaBabySelector() {
+  $(document).ready(function() {
+    output = $('#baby-ninja');
+  });
+  return output;
+}
+
+function divSelector() {
+  $(document).ready(function() {
+    output = $('.pics');
+  });
+  return output;
+}
+
+function firstListItem() {
+  $(document).ready(function() {
+    output = $('#pic-list li:first-child');
+  });
+  return output;
+}
+
