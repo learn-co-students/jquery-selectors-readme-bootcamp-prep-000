e162b69634dd5a349ac0a41b60cabbb73fb59599
Mendy Wolosow1 <mendywoly+1@gmail.com>
2018-03-29 20:37:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  ptags = $('p')
+  return ptags  
+}
+
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function  ninjaBabySelector() {
+ return  $('#baby-ninja')
+  
+}
+
+
+function divSelector() {
+  return $('.pics')
+}
+
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
