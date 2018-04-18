45cefa9ff771506e4f66921ad6b926ec9fa15e03
Lorena <lorena.giddens@outlook.com>
2018-04-18 05:29:14 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+// element selector
+function paragraphSelector() {
+  return $('p');
+}
+
+// last selector
+function lastImageSelector() {
+  return $('img:last');
+}
+
+// ID selector
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+// class selector
+function divSelector() {
+  return $('.pics');
+}
+
+// first-child selector
+function firstListItem() {
+  return $('#pic-list li:first-child');
+}
