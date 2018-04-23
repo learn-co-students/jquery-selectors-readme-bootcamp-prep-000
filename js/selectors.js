7a34c7da46b5b48de6af8f272ddec798155adb14
Brindha S <brinsree@gmail.com>
2018-04-23 21:35:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  return($('p'));
+}
+function lastImageSelector(){
+  return ($('ul li:last-child img'));
+}
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+function divSelector(){
+  return $('.pics');
+}
+function firstListItem(){
+return $('#pic-list li:first-child');
+}
