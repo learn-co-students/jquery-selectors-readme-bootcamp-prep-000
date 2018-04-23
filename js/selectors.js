b00f64df0298c8e01b6def943b7da282961c4c7a
joseph.chesir@mail.yu.edu <joseph.chesir@mail.yu.edu>
2018-04-23 19:19:51 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
-// declare your functions here...
+function paragraphSelector() {
+   $('p')
+   return $('p')
+}
+
+function lastImageSelector() {
+  $('img:last')
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  $('#baby-ninja')
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  $('.pics')
+  return $('.pics')
+}
+
+function firstListItem() {
+  $('.pics ul#pic-list li:first-child')
+  return $('.pics ul#pic-list li:first-child')
+  
+}
