daddf85ac2fc6451f2e8746e6788deb5c8789889
Cory Pavitt <corypavitt@gmail.com>
2018-04-23 17:07:09 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var select = $('p');
+  return select
+}// declare your functions here...
+
+
+function lastImageSelector() {
+    return $('img:last')
+
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+
+}
+
+function divSelector() {
+  return $('.pics')
+
+}
+
+function firstListItem() {
+    return $('ul li:first-child')
+}
