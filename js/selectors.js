baf3a00a6b0712839ae7aa727161be5e3fe418f3
Newton Migosi <newtonmigosi@gmail.com>
2018-04-25 17:47:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('#pic-list li:last-of-type img');
+}
+
+function  ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $(".pics");
+}
+
+function firstListItem() {
+  return $("#pic-list li:first-child");
+}
