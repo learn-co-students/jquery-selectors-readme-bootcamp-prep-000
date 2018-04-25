38d85bf51acce93ebc4110623c52f9d1b1b0b420
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-04-25 14:34:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $('p')
+  return p
+}
+
+function lastImageSelector() {
+  var p = $('img:last')
+  return p
+}
+
+function ninjaBabySelector() {
+  var p = $('#baby-ninja')
+  return p
+}
+
+function divSelector() {
+  var p = $('.pics')
+  return p
+}
+
+function firstListItem() {
+  var p = $('#pic-list li:first-child')
+  return p
+}
