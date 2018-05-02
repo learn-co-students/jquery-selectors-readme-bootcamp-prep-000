98002ea6375d750c2038fdde854aef48af5cd90a
Janice Darikho <janicedarikho@gmail.com>
2018-05-02 14:14:06 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $("img[src='https://s3.amazonaws.com/after-school-assets/baby-ninja.gif']");
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $("#pic-list li:first-child");
+}
