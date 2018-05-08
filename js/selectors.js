2034c1fdc0e46d5973709fb939301f69ed9b9a1e
Mark Carr <markcarrmusic@gmail.com>
2018-05-08 17:21:59 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  const paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  const lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  const babyNinja = $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector() {
+  const divPic = $('div.pics');
+  return divPic;
+}
+
+function firstListItem() {
+  const firstList = $('ul#pic-list li:first-child');
+  return firstList;
+}
