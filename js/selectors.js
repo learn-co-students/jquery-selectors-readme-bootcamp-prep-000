cc680081fb3fc09b61381f1846ef693a1a68aeb2
caida.ding@baruchmail.cuny.edu <caida.ding@baruchmail.cuny.edu>
2018-05-14 01:29:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $("p");
+}
+
+function lastImageSelector(){
+  return $("img:last")
+}
+
+function ninjaBabySelector(){
+  return $("#baby-ninja")
+}
+
+function divSelector(){
+  return $(".pics:first-child") + $(".pics:second-child")
+}
+
+function firstListItem(){
+  return $("#pics.list ul li:first-child")
+}
