32090726723e960692d771ab55778f9751aba7ff
Juliana Kang <julianajlk@gmail.com>
2018-05-16 01:27:09 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  var selectors = $("p");
+  return selectors;
+};
+
+function lastImageSelector() {
+  var lastImg = $('div img:last');
+  return lastImg;
+};
+
+function ninjaBabySelector() {
+  var ninjaBabyImg = $('#baby-ninja');
+  return ninjaBabyImg
+};
+
+function divSelector() {
+  var classPic = $('div[class="pics"]');
+  return classPic;
+};
