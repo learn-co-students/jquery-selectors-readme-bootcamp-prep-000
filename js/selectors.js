c3c9f58d76235969be7738d5d998e09afd1613a8
Christopher Stull <chris.stull@gmail.com>
2018-05-28 20:45:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('img[id="baby-ninja"]')
+}
+
+function divSelector(){
+  return $("div[class='pics']")
+}
+
+function firstListItem(){
+  return $("ul[id='pic-list'] li:first-child")
+}
