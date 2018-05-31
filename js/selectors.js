84bbae48247d61f36d06ea6e7e0b95e00885ea57
Maxim Dalton <maximcdalton@gmail.com>
2018-05-31 05:09:31 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+var paragraphSelector = function() {
+  return $('p')
+};
+
+var lastImageSelector = function() {
+  return $('img:last');
+};
+
+var ninjaBabySelector = function() {
+  return $('#baby-ninja');
+};
+
+var divSelector = function() {
+  return $('.pics');
+};
+
+var firstListItem = function() {
+  return $('#pic-list li:first-child');
+};
