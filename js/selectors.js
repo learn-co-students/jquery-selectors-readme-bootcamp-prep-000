404799f2f353c12808519ed5e45365a830dd0996
Edward Mixon <edwardchase@gatech.edu>
2018-06-04 21:26:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  var p = $('p')
+  return p;
+}
+
+function lastImageSelector() {
+  var last = $('img:last');
+  return last;
+}
+
+function ninjaBabySelector() {
+  var baby = $('#baby-ninja');
+  return baby;
+}
+
+function divSelector() {
+  var d = $('.pics');
+  return d;
+}
+
+function firstListItem() {
+  var first = $('#pic-list li:first-child');
+  return first;
+}
