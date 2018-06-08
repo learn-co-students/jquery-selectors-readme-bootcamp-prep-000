677e1b4f77d6d2a036335b5e7e4d6fb84f529ff2
NaKia Whitby <whitby.nakia@gmail.com>
2018-06-08 18:02:39 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,15 @@
-// declare your functions here...
+function paragraphSelector () {
+  return $('p')
+}
+
+function ninjaBabySelector () {
+  return $('#baby-ninja')
+}
+
+function divSelector () {
+  return $('.pics')
+}
+
+function firstListItem () {
+  return $('#pic-list li:first');
+}
