cdb7caa3636b6b2eee11a6b4d60eccad3b44cc23
Matthew Young <myoung162744@gmail.com>
2018-06-10 15:18:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('div img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  return $('ul #pic-list: first-child')
+}
