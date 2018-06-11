003f7b717c30858a0821bafd2285bce196e652ef
Alex Edwards <info@acedigitalagency.net>
2018-06-11 18:50:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+Function paragraphSelector() {
+$('p');
+return
+}
+
+Function lastImageSelector() {
+$('img:last');
+return
+}
+
+Function ninjaBabySelector() {
+$('#baby-ninja');
+return
+}
+
+Function divSelector() {
+$('.pics');
+return
+}
+
+Function firstListItem() {
+$('#pic-list li:first-child');
+return
+}
