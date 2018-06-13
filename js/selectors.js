d33c4bbe011358bd79494f5dce01d1f44ba50df6
Shravani PL <pl.shravani@gmail.com>
2018-06-13 22:41:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  var arr = $('p')
+  return arr
+}
+
+function lastImageSelector() {
+ return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child')
+}
