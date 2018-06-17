788424c8a37c296493166767e2f6447ff258d757
Phillip Green <phillycheez11@gmail.com>
2018-06-17 23:06:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p').return;
+}
+
+function lastImageSelector(){
+  $('img:last').return;
+}
+
+function ninjaBabySelector(){
+  $('baby-ninja').return;
+}
+
+function divSelector(){
+  $('#pics').return;
+}
+
+function firstListItem(){
+  $('div img:first-child').return;
+}
