f58157a11d85c0140a0b11263ab0782bf2db6db9
Matthew McCormack <mmccormack1182@gmail.com>
2018-06-17 01:16:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  $(`p`)
+}
+
+function lastImageSelector() {
+  $(`img:last`)
+}
+
+function ninjaBabySelector() {
+  $(`#baby-ninja`)
+}
+
+function divSelector() {
+  $(`.pics`)
+}
+
+function firstListItem() {
+  $(`ul #pic-list:first-child`)
+}
