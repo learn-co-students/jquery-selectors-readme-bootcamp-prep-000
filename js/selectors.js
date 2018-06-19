c8480945542eef05a1fc5a8b00e6d6ae195c9cbd
Joseph Lander <josephclander@gmail.com>
2018-06-19 08:31:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+function paragraphSelector() {
+  el = $('p');
+  return el;
+}
+
+function lastImageSelector() {
+  el = $('img:last');
+  return el;
+}
+
+function ninjaBabySelector() {
+  el = $('#baby-ninja');
+  return el;
+}
+
+function divSelector() {
+  el = $('div.pics');
+  return el;
+}
+
+function firstListItem() {
+  el = $('ul#pic-list li:first-child');
+  return el;
+}
+
+
+
+
+
