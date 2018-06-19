2699418c567400161a64a0c0820b3af911130784
gmat191@gmail.com <gmat191@gmail.com>
2018-06-19 22:32:42 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $(".pic:nth-child(1)")+$(".pic:nth-child(2)")
+} 
+
+function firstListItem() {
+  return $("#pic-list li:first-child")
+}
+
