85ab0bf9cea03cdebd66c9013c9c001e2bf1343e
joannewilkins88@gmail.com <joannewilkins88@gmail.com>
2018-06-20 11:15:59 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+
+/*function paragraphSelector() {
+  var parSelect = $('p');
+  var allPar = [];
+  for (let i = 0; i<parSelect.length;i++) {
+  var currPar = parSelect[i].innerHTML
+  allPar.push(currPar);
+  }
+}   */
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+   return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+ return $('.pics')
+}
+
+function firstListItem() {
+ return $('#pic-list li:first-child')
+}
