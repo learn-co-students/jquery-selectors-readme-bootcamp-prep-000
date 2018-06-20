0db6bf297c271f1dfbb01fc9e885aec542486019
nmkleiner@gmail.com <nmkleiner@gmail.com>
2018-06-20 07:42:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,19 @@
 function paragraphSelector() {
   return $('p')
 }
+
+function lastImageSelector () {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
