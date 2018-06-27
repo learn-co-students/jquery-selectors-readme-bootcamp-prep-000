eb0b8365a1a0f718c5a17c85d09bdbe83c1392a3
Lige <lige.williamson@gmail.com>
2018-06-27 12:51:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector() {
+  const paragraphs = $('p');
+  return paragraphs;
+};
+
+function lastImageSelector() {
+  const lastImage = $('img:last');
+  return lastImage;
+};
+
+function ninjaBabySelector() {
+  const babyNinja = $('#baby-ninja');
+  return babyNinja;
+};
+
+function divSelector() {
+  const pics = $('.pics');
+  return pics;
+};
+
+function firstListItem() {
+  const firstChild = $('#pic-list li:first-child');
+  return firstChild;
+};
