8e3e45f92eee3f855994f6f3c0dfc8a8727f50a1
Christopher Rouxel <chris.rouxel88@gmail.com>
2018-06-28 16:21:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
-// declare your functions here...
+$('body').prepend(firstListItem())
+var ans=firstListItem()
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first')
+}
