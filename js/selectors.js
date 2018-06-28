0049e85874dcb5dfbc7e2212143ccbc1aeab5f3e
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-06-28 18:50:27 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,9 +1,20 @@
 function paragraphSelector(){
- $('p');
- return ('p');
+ return $('p');
 }
 
+function lastImageSelector(){
+  return $('img:last');
+}
 
+function ninjaBabSelector(){
+  return $('#baby-ninja')
+}
 
+function divSelector(){
+  return $('.pics');
+}
 
+function firstListItem(){
+  return $('ul#pic-list li:first-child');
+}
 // declare your functions here...
