ae0e5612526267ff8c1b6174582c372f0323dd59
Lung Chen <lungheichen@gmail.com>
2018-06-29 14:47:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,41 @@
 // declare your functions here...
+$(document).ready(function() {
+
+  function paragraphSelector() {
+    return $('p')
+  }
+
+  function lastImageSelector() {
+    return $('img:last')
+  }
+
+  function ninjaBabySelector() {
+    return $('#baby-ninja')
+  }
+
+  function divSelector() {
+    return $('div .pics')
+  }
+
+  function firstListItem() {
+    return $('ul:first-child #piclist')
+  }
+
+  // for tags
+  $('img')
+
+  // for class
+  $('.pics')
+
+  // for ids
+  $('#baby-ninja')
+
+  // select first child of parent
+  $('parent-tag child-tag:first-child')
+
+  //select based on alt info
+  $("img[alt='the beatles making faces']")
+
+  // last selector, "last"
+  $('div:last')
+});
