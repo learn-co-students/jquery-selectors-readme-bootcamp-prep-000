cce885fdcfabd2e6b313513f81b259239a37993b
Corey H <c.l.halicke@gmail.com>
2018-07-02 22:57:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  para = $('p')
+  return para
+}
+
+
+function lastImageSelector() {
+  last = $('img:last')
+  return last
+}
+
+
+function ninjaBabySelector() {
+  ninja = $('#baby-ninja')
+  return ninja
+}
+
+
+function divSelector() {
+  div = $('.pics')
+  return div
+}
+
+function firstListItem() {
+  first = $('ul#pic-list li:first-child')
+  return first
+}
