02f4fd8803861a6089f8a77595c8c771ee7e975d
Felix Chan <felixchan29@gmail.com>
2018-07-05 16:19:01 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  console.log($('ul#pic-list li:first-child'));
+  return $('ul#pic-list li:first-child');
+}
