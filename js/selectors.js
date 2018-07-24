dfb7996ce05cc82ccac595e627c2b9f1d812f3ed
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-24 02:11:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector () {
+  let paragraphs = $('p').get()
+  return paragraphs
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics').get()
+}
+
+function firstListItem() {
+  return $('ul#pic-list > li').first()
+}
