8e6a34fef4d4560a88fc0d7b04e49b66d17fa6c5
mjcabrera@gmail.com <mjcabrera@gmail.com>
2018-07-25 19:21:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
+
+//The function should use a first-child selector to return the first list item in the ul with the ID pic-list.//
