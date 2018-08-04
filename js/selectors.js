dd69c6badd4a8c1386a425bc78ace6c5e328e2cf
Mike Dilley <mike.dilley@gmail.com>
2018-08-04 15:20:12 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $("p");
+  return p;
+}
+
+function lastImageSelector() {
+  var img = $("img:last");
+  return img;
+}
+
+function ninjaBabySelector() {
+  var ninja = $("#baby-ninja");
+  return ninja;
+}
+
+function divSelector() {
+  var div = $(".pics");
+  return div;
+}
+
+function firstListItem() {
+  var first = $("#pic-list li:first-child")
+  return first;
+}
