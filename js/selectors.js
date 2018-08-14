add33ac82de94c26efcf58147e23f6bc55b2df35
Caitlin Lauer <caitlinrinn@gmail.com>
2018-08-14 14:36:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraph = $("p");
+  return paragraph;
+}
+
+function lastImageSelector() {
+  var images = $("img:last");
+  return images;
+}
+
+function ninjaBabySelector() {
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector() {
+  var classP = $('.pics');
+  return classP;
+}
+
+function firstListItem() {
+  var firstchild = $('ul li:first-child');
+  return firstchild;
+}
