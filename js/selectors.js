6eee48ceb1444f4ebd148baa633443211895e12a
Lee Simon <shinoblee@gmail.com>
2018-08-20 19:49:17 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let items = $('p');
+  return items;
+}
+
+function lastImageSelector(){
+  let image = $('div img:last');
+  return image;
+}
+
+function ninjaBabySelector(){
+  let image = $('#baby-ninja');
+  return image;
+}
+
+function divSelector(){
+  let pics = $('.pics');
+  return pics;
+}
+
+function firstListItem(){
+  let pic = $('li:first');
+  return pic;
+}
