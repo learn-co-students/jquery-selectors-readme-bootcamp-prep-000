6cd0e70323af2e6497cfc69c8d3dd037d0661d55
Kaveh Saleminejad <lcaveh@gmail.com>
2018-08-20 18:54:36 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+  const paragraph = $('p');
+  return paragraph;
+}
+function lastImageSelector() {
+  const lastImage = $('img:last');
+  return lastImage;
+}
+function ninjaBabySelector() {
+  const ninjaImage = $('img#baby-ninja');
+  return ninjaImage;
+}
+function divSelector() {
+  const pics = $('div.pics');
+  return pics;
+}
+function firstListItem() {
+  const pics = $('ul#pic-list li:first-child');
+  return pics;
+}
