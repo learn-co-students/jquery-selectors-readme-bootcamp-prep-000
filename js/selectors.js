66d20e9339f6578e85c3df643f21bf31ddcb0bc8
jvpink200 <jvillarr20@gmail.com>
2018-08-21 23:16:57 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector(){
+  var p = $("p");
+  return p;
+}
+
+function firstListItem(){
+  var item = $("ul li:first-child");
+  return item;
+}
+function lastImageSelector(){
+  var img = $("img:last");
+  return img;
+}
+
+function divSelector(){
+  var div = $(".pics");
+  return div;
+}
+
+function ninjaBabySelector(){
+  var ninja = $("img[id='baby-ninja']");
+  return ninja;
+}
