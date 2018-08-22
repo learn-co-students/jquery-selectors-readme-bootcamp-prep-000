e5072786f996829b014a62c17561e22e4caa2709
Jean-Patrice <jpjocegilbert@gmail.com>
2018-08-22 16:17:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+//function should use an element selector to select the p tags in index.html and return those elements.
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('img#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('ul#pic-list li:first-child')
+}
