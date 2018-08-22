d0ae403408b92bcdd7c256ca223f65501a4b45ea
ahmed atta <ahmedatta3322@gmail.com>
2018-08-22 19:03:34 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+function divSelector(){
+  return $('.pics')
+}
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
+function lastImageSelector(){
+  return $('img:last')
+}
