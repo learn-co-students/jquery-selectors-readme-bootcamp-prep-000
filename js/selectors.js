464a7df881d679b8c9f9933e9cd11743b256cf1e
Ian Harrison <ianmartinharrison@outlook.com>
2018-08-25 09:54:25 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  paragraphs = $('p')
+  return paragraphs
+}
+
+function lastImageSelector() {
+  lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  ninjaBaby = $('#baby-ninja')
+  return ninjaBaby
+}
+
+function divSelector() {
+  pics = $('.pics')
+  return pics
+}
+
+function firstListItem() {
+  firstChild = $('#pic-list li:first-child')
+  return firstChild
+}
