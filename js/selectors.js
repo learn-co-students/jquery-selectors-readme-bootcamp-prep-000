3c79f16a94db4adffba77546a8769175fb4d382c
Sam B <s77broz@aol.com>
2018-09-01 04:19:55 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector() {
+  return $("p");
+}
+
+function lastImageSelector() {
+  return $("ul#pic-list li:last-child img");
+}
+
+function ninjaBabySelector() {
+  return $("#baby-ninja");
+}
+
+function divSelector() {
+  return $(".pics");
+}
+
+function firstListItem() {
+  return $("ul#pic-list li:first-child");
+}
