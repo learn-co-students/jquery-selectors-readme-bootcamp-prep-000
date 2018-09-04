3b977e343ccf5b00e48b81ea36d4ae08057592a3
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-04 00:50:17 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+const x =  $('p');
+return x;
+}
+
+function lastImageSelector (){
+ return $('img:last')
+}
+
+function ninjaBabySelector (){
+  return $('#baby-ninja')
+}
+
+function divSelector (){
+  return $('.pics')
+}
+
+function firstListItem (){
+  return $('ul li:first-child')
+}
