d21746602041ca71f03ca1b33e0e17b387915ca7
yisangho <yisangho717@gmail.com>
2018-09-08 05:52:04 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+$('selector-goes-here')
+
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('#pic-list li:first')
+}
