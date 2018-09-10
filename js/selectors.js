d1d84581e3d2f4decaa64f0e916473e96d7d1c94
demian sims <demiansims@gmail.com>
2018-09-10 02:28:09 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector() {
+  var p = $('p');
+  return p 
+}
+
+function lastImageSelector() {
+  var lastImg = $('img:last');
+  return lastImg
+}
+function ninjaBabySelector() {
+  var babyNinja = $('#baby-ninja');
+  return babyNinja 
+}
+
+function divSelector() {
+  var divSelect = $('div.pics');
+  return divSelect
+}
+
+function firstListItem() {
+  var firstList = $('ul#pic-list li:first-child');
+  return firstList 
+}
