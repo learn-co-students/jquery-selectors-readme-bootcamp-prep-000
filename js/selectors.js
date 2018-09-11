4da2f8dc0bd947b00de5c95da4e4cf8b7fbb4fa7
Omar A <oazzat19@gmail.com>
2018-09-11 16:58:06 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  var pTag = $('p')
+  return pTag
+}
+
+function lastImageSelector(){
+  var lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector(){
+  var ninBaby = $('#baby-ninja')
+  return ninBaby
+}
+
+function divSelector(){
+  var selec = $('.pics')
+  return selec
+}
+
+function firstListItem(){
+  var fir = $('#pic-list li:first-child')
+  return fir
+}
