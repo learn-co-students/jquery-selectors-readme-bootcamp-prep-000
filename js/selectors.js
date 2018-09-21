7b460ab19f398b0fe6572b4800a2d6105cc56a8f
Jonnel Benjamin <jonnel.benjamin@gmail.com>
2018-09-21 16:47:56 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  var allParagraphs = $('p')
+  return allParagraphs
+}
+
+function lastImageSelector(){
+ return  $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first')
+}
