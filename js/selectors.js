dc23738bc0f8ca1cb832321abeb1f0a43327ac71
Andrew Johnson <ajohnson.uva@gmail.com>
2018-09-25 19:11:24 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector () {
+  var allPTags = $('p');
+  return allPTags;
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector () {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector () {
+  var divClassPics = $('div.pics');
+  return divClassPics;
+}
+
+function firstListItem () {
+  var picInUl = $('ul#pic-list li:first-child');
+  return picInUl;
+}
