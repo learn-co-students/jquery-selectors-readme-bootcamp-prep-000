44fae70b011af10c0bbbf21cc5a4ebd232b6ee35
Skylar Salerno <skylar.salerno@gmail.com>
2018-09-27 21:12:31 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,6 @@
 // declare your functions here...
+function paragraphSelector(){return $('p')}
+function lastImageSelector(){return $('img:last');}
+function divSelector(){return $('.pics');}
+function firstListItem(){return $('#pic-list li:first')}
+function ninjaBabySelector(){return $('#baby-ninja');}
