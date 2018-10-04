70a084237bc98ae0120bc2faeeab66bc6fc44644
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 22:16:44 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+$(document).ready( ()=> {
+  mocha.run()
+})
+
+function paragraphSelector() {
+  //The function should use an element selector to select the p tags in index.html and return those elements. (Note that we're using the plural because the selector will always return an array when used with tags, even if there's only one!)
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul li:first')
+}
