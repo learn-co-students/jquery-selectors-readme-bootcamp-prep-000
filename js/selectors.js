b38101226cf9c97cd28bb338d28745340911530c
Matthew Sherman <msherman0801@gmail.com>
2018-10-06 14:22:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+function paragraphSelector() {
+  
+var test = $('p');
+return test;
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
