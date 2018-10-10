f709ef980b951c6fbfcc70d9828c37180081a9e6
John Hayes <johnnyhayes7@gmail.com>
2018-10-10 18:21:47 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+ var p = $('p');
+ return p;
+}
+
+function lastImageSelector(){
+  var last = $('img:last');
+  return last;
+}
+
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja');
+  return ninja;
+}
+
+function divSelector(){
+  var divs = $('.pics');
+  return divs;
+}
+
+function firstListItem(){
+  var first = $('ul li:first-child');
+  return first;
+}
