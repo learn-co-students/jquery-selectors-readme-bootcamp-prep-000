8b4b52b9ce1092e0906e2630bc5e61d3c7811289
mssigler <mssigler@gmail.com>
2018-10-14 22:30:26 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return($('p')); 
+}
+
+function lastImageSelector(){
+ return( $('img:last') ); 
+}
+
+function ninjaBabySelector(){
+  return($('#baby-ninja'))
+}
+
+function divSelector(){
+  return ( $('.pics') )   //removed '.pics div'
+}
+
+function firstListItem(){
+  return( $('#pic-list li:first-child' ));
+  
+  // tried  'ul li #pic-list:first-child '
+  // tried  'li #pic-list:first-child '
+  // tried  'ul #pic-list:first-child '
+  // tried  'ul li:first-child #pic-list'
+  // tried  '$ul'  ,($('li #pic-list:first-child' 
+  // tried   $('ul:first-child'), $('#pic-list') );
+}
+
