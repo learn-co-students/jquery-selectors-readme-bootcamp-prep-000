c06ea7c44f45c4cfd38ed8f4e255a9427b3fc945
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2018-10-21 02:28:59 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector () {
+   var pTags = $('p');
+   return pTags;
+}
+
+function lastImageSelector () {
+  var lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector () {
+  var coolBaby = $('#baby-ninja');
+  return coolBaby;
+}
+
+function divSelector () {
+  var twoPics = $('.pics');
+  return twoPics;
+}
+
+function firstListItem () {
+  var listOne = $('#pic-list li:first-child');
+  return listOne;
+}
