a7e7b4be593a523b7fe23879afc453edb9daee6c
Brian <briandsalemi@gmail.com>
2018-11-06 00:36:15 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,11 @@
 // declare your functions here...
+
+function paragraphSelector() { 
+ const paras = document.getElementsByTagName('p');
+  return paras;
+}
+
+function lastImageSelector() {
+  last = $('img:last');
+  return last;
+}
