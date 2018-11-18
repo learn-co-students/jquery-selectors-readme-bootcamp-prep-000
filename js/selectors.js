2a446c1b44d5b1d56c501224fa0f0594c8e336f6
Raymond Chow <raychow1944@yahoo.com>
2018-11-18 00:23:10 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+//$(document).ready(function() {
+  function paragraphSelector() {
+    return $('p');
+  }
+  
+  function lastImageSelector() {
+    return $('img:last');
+  }
+  
+  function ninjaBabySelector() {
+    return $('#baby-ninja');
+  }
+  
+  function divSelector() {
+    return $('div.pics');
+  }
+  
+  function firstListItem() {
+    return $('#pic-list li:first-child');
+  }
+//});
