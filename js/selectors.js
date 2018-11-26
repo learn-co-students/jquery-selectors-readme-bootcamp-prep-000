77d028f2b5343a64789bd3ae005785177e79fa87
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-26 12:36:36 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  var para = $("p");
+  return para;
+}
+
+function lastImageSelector(){
+  var lastImg = $("img:last");
+  return lastImg;
+}
+
+function ninjaBabySelector(){
+  var ninja = $("#baby-ninja");
+  return ninja;
+}
+
+function divSelector(){
+  var content = $(".pics");
+  return content;
+}
+
+function firstListItem(){
+  var firstItem = $("ul li:first-child");
+  return firstItem;
+}
