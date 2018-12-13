3fb1c4353ff4bf2512c36e8d705b0dd03af66f27
OSCAR RIOS <oscr.rss@gmail.com>
2018-12-13 04:41:23 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -7,3 +7,9 @@ function lastImageSelector(){
 function ninjaBabySelector(){
   return $("#baby-ninja")
 }
+function divSelector(){
+  return $(".pics")
+}
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
