6a87098e177cbcf40ca56a134e387e2b95ff7441
OSCAR RIOS <oscr.rss@gmail.com>
2018-12-13 04:00:35 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,9 @@
-// declare your functions here...
+function paragraphSelector(){
+  return $("p")
+}
+function lastImageSelector(){
+  return $("img:last")
+}
+function ninjaBabySelector(){
+  return $("#baby-ninja")
+}
