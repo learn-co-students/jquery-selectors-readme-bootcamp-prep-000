6e871264362e5826b10e88b915f420ab68c8ff39
richardtam.ny <richardtam.ny@gmail.com>
2018-12-14 21:18:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  var x = $('p')
+  return  x
+}
+
+function lastImageSelector() {
+  var x = $('img:last')
+  return x
+}
+
+function ninjaBabySelector() {
+  var x = $('#baby-ninja')
+  return x
+}
+
+function divSelector() {
+  var x = $('.pics')
+  return x
+}
+
+function firstListItem() {
+  var x = $('ul li:first-child')
+  return x
+}
