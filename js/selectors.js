0f60f40bb3383ae7b404eefbf29fcf5bd79b0f84
Maximillian Schulte <maximilliangschulte@gmail.com>
2018-12-19 04:13:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,18 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+function lastImageSelector(){
+  return $('img:last')
+}
+function ninjaBabySelector(){
+  return $('#babyninja')
+}
+function divSelector(){
+  var divs = $('div')
+  return divs.slice(0,2)
+}
+function firstListItem(){
+  var tags = $('li')
+  return tags[0]
+}
