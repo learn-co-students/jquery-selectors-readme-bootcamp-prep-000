e34c2555deb08b589a7b516a7c63de474291b63d
Alex Holhos <alex.holhos@emanuel.ro>
2018-12-20 13:31:32 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+paragraphSelector = function(){
+  return $('p');
+}
+lastImageSelector = function(){
+  return $('img').last();
+}
+ninjaBabySelector = function(){
+  return $('#baby-ninja');
+}
+divSelector = function(){
+  return $('div.pics');
+}
+firstListItem = function(){
+  return $('#pic-list li').first();
+}
