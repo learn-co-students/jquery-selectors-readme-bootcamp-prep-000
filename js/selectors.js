942261b0305543367444f24e2ff616749f6ee695
Travis Martin <travisemerymartin@gmail.com>
2018-12-27 22:42:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraphs = $('p');
+  return paragraphs
+}
+
+function lastImageSelector() {
+  var image = $('img:last');
+  return image
+}
+
+function ninjaBabySelector() {
+  var binja = $('body div img:first');
+  return binja
+}
+
+function divSelector() {
+  var pics = $('.pics');
+  return pics
+}
+
+function firstListItem() {
+  var firstPic= $('.pics #pic-list:first-child');
+  return firstPic
+}
