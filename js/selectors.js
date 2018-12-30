94795c9b43d515dbe4944c8045b34c66792e34d0
Emmanuel Anzele <eanzele@gmail.com>
2018-12-30 21:05:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,17 @@
-// declare your functions here...
+// declare your functions her
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+ return $('img:last');
+}
+function ninjaBabySelector() {
+ return $("img[id='baby-ninja']");
+}
+function divSelector() {
+  return $('.pics');
+}
+function firstListItem() {
+  return $("ul[id='pic-list'] li:first-child");
+}
