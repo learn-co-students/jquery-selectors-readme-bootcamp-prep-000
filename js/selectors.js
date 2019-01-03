205cb07ffb9a66c345bcaae1ddb3953baaf0d05b
eselbeus <eselbeus@gmail.com>
2019-01-03 02:54:49 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let para = $("p");
+  return para;
+}
+
+function lastImageSelector(){
+  let lastImg = $("img:last");
+  return lastImg;
+}
+
+function ninjaBabySelector(){
+  let ninjaImg = $("#baby-ninja");
+  return ninjaImg;
+}
+
+function divSelector(){
+  let picsDiv = $(".pics");
+  return picsDiv;
+}
+
+function firstListItem(){
+  let first = $("ul li:first-child");
+  return first;
+}
