bc9111d1cd4ba7ebf10cd0a8bd1927dc5b57ca73
matthew t muniz-silva <mmunizsi01@gmail.com>
2019-01-08 16:06:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  let p = $('p');
+  return p;
+}
+
+function lastImageSelector(){
+  let lastImg = $('img:last');
+  return lastImg;
+}
+
+function ninjaBabySelector(){
+  let ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector(){
+  let twoDiv = $('div');
+  return twoDiv;
+}
+
+function firstListItem(){
+  let firstItem = $('div ul li:first-child');
+  return firstItem;
+}
