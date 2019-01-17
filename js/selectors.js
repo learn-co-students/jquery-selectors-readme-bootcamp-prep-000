a18703f32f5b685f24b225d91fa14916702b9641
Paris Dolfman <parisdolfman@gmail.com>
2019-01-17 21:27:31 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,14 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  //code here
+}
+
+
+function lastImageSelector () {
+  // usa last jQuery selector to return last image in index.html
+}
+
+function ninjaBabySelector () {
+  // use ID selector to return ninja baby image
+}
