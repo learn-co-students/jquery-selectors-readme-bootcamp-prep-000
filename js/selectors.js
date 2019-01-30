5b3320e5d523bf10fc4b82d7d6c4d735ddf1e49b
Jonathan Koss <jonathan_koss@yahoo.com>
2019-01-30 06:25:43 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -6,3 +6,16 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last');
 }
+
+function ninjaBabySelector() {
+  return $('#baby-ninja'); 
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul li:first-child');
+}
+
