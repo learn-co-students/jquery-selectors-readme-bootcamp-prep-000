a2ff1aa83d80eefa956f50c40cbb12ac6b4ad482
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-04 06:03:51 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 // declare your functions here...
+
+//$(document).ready(function() {
+
+function paragraphSelector(){
+  var p = $('p')
+  return p
+}
+
+function lastImageSelector(){
+  var last = $('img:last')
+  return last
+}
+
+function ninjaBabySelector(){
+  var ninja = $('#baby-ninja')
+  return ninja
+}
+ function divSelector(){
+   var pics = $('.pics')
+   return pics
+ }
+function firstListItem(){
+  var ul = $('#pic-list li:first-child')
+  return ul
+}
+  
+  
+//});
