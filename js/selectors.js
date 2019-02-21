c95a38cce5d35044c7ce1235f510b6949b0452be
Monika Tadross <monikk.tadross@gmail.com>
2019-02-21 00:27:43 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,33 @@
 // declare your functions here...
+function paragraphSelector() {
+  var element = $('p');
+    return element;
+ } 
+  
+
+
+function lastImageSelector() {
+  var element = $('img:last');
+  return element;
+}
+
+
+
+function ninjaBabySelector() {
+  var element = $('#baby-ninja');
+  return element;
+}
+
+
+
+function divSelector() {
+  var element = $('.pics');
+  return element;
+}
+
+
+
+function firstListItem() {
+  var element = $('ul#pic-list li:first-child');
+  return element;
+}
