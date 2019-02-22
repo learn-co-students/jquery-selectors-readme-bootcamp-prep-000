da837deda581f1a7e9c4a7fceaf1f118058742da
brendanfrrs Ferris <brendanfrrs@gmail.com>
2019-02-22 16:21:50 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  var v = $('p');
+  return v;
+}
+
+function lastImageSelector(){
+  var v = $('img:last')
+  return v;
+}
+
+function ninjaBabySelector(){
+  var v = $('#baby-ninja');
+  return v;
+}
+
+function divSelector(){
+  var v = $(".pics");
+  return v;
+}
+
+function firstListItem(){
+  var v = $("ul li:first-child");
+  return v;
+}
