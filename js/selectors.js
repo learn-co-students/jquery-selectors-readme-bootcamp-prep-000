1b4f3d9ac304306b00cfebd90e879cc2036de909
Spencer Lindemuth <lindemut@gmail.com>
2019-03-01 22:25:51 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,10 @@
 // declare your functions here...
+function paragraphSelector(){
+  var ps = $('p')
+  return ps
+}
+
+function lastImageSelector(){
+  var images = $('img:last')
+  return images
+}
