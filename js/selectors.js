822d63c8cf52c5db52e69021f6157ff9bb49d681
Spencer Lindemuth <lindemut@gmail.com>
2019-03-01 22:37:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -8,3 +8,19 @@ function lastImageSelector(){
   var images = $('img:last')
   return images
 }
+
+
+function ninjaBabySelector(){
+  ninja = $('#baby-ninja')
+  return ninja
+}
+
+function divSelector(){
+  divs = $('.pics')
+  return divs
+}
+
+function firstListItem(){
+  first = $('#pic-list li:first-child')
+  return first
+}
