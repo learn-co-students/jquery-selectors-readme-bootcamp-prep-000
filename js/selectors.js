6c84d625ed94b1e6be94d318de4e5aaa6ed9fdb5
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-15 03:20:20 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,29 @@
 // declare your functions here...
+function paragraphSelector()
+{
+  return $('p');
+}
+
+function lastImageSelector()
+{
+  return $('img:last');
+}
+
+function ninjaBabySelector()
+{
+  return $('#baby-ninja');
+}
+
+function divSelector()
+{
+  const a = $('div');
+  return [a[0] , a[1]];
+  //return [$('div:first') , $('div:last')];
+}
+
+function firstListItem()
+{
+  const a = $('ul li');
+  return [a[0]];
+  //return $('ul li')[0];
+}
