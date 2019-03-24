9164bcd7f3d942777f9cc25e5d435c67ec21f755
Ay Has <ahashim983@yahoo.com>
2019-03-24 21:22:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
+$('img')
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('ul li:first-child')
+}
 // declare your functions here...
