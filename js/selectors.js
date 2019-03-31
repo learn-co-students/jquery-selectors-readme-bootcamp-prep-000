335bb702d4ba98accd60a9275ba8497192210a17
brandon corey <bcorey85@gmail.com>
2019-03-31 18:59:18 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('img:last')
+}
+
+function ninjaBabySelector(){
+  return $('img#baby-ninja')
+}
+
+function divSelector(){
+  return $('div.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
