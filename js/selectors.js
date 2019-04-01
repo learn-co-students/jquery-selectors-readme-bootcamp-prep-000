ae76e9dde3732e24d986b393e2a41eb1c810a594
Dale Sublett <djsublett04@gmail.com>
2019-04-01 20:32:37 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+$('div img:first-child')
+
+$("img[alt='the beatles making faces']");
+
+function paragraphSelector() {
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function firstListItem() {
+  return $('ul > li:first-child')
+}
+
