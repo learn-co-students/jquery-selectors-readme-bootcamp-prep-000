e950981f830e447f37ddb04637a8426d42f4257e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-02 02:50:28 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -6,3 +6,15 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last');
 }
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('ul#pic-list li:first-child');
+}
