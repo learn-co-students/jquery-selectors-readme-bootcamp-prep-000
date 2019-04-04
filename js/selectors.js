2792837512d4151b9fb5a4efc39b79a5edd261df
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-04 02:41:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -2,3 +2,13 @@
 function paragraphSelector() {
   return $('p');
 }
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+
