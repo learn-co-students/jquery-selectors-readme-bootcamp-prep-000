d8d2aebbe4f20b139ac67de0c93800c1a14e257c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-04-10 17:50:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,3 +1,26 @@
 // declare your functions here...
 
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  //$('parent-tag child-tag:first-child')
+  //$('div img:first-child')
+  
+  return $('ul li:first-child');
+}
+
 
