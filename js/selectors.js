a87aa61e693f7d8b919ede68a4f8c7c0c58ee7b8
Keith Burgie <krburgie@gmail.com>
2019-04-22 05:17:03 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+  let paragraphs = $('p');
+  return paragraphs;
+}
+
+function lastImageSelector() {
+  let lastImage = $('img:last');
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  let ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  let pics = $('.pics');
+  return pics;
+}
+
+function firstListItem() {
+  let firstListItem = $('#pic-list li:first-child');
+  return firstListItem;
+}
