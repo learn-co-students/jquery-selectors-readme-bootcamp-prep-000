3ee159821b7038ac410ec3d28dc13e6907a4679c
Beto Dute <betodute@gmail.com>
2019-05-13 07:01:05 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector(){
+  $('p');
+}
+
+function lastImageSelector(){
+  $('img:last');
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja');
+}
+
+function divSelector(){
+  $('.pics');
+}
+
+function firstLineItem(){
+  $('ul pic-list:first-child');
+}
