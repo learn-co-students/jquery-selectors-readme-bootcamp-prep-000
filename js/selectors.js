336788a92794dea42cfc53f378fa875dc2aa05db
Nekwan Luke <contact@nekwanluke.com>
2019-05-18 22:40:08 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector () {
+  return $('p')
+}
+
+function lastImageSelector () {
+  return $('img:last')
+}
+
+function ninjaBabySelector () {
+  return $('#ninja-baby')
+}
+
+function divSelector () {
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
