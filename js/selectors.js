60de35acfa8bd3c86e3c03bbf1681212772a47ef
communal-principle-3181 <aellis19@outlook.com>
2019-05-27 21:02:43 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,30 @@
 // declare your functions here...
+function paragraphSelector ()
+{
+  var paragraph = $('p');
+  return paragraph;
+}
+
+function lastImageSelector()
+{
+  var image = $('img');
+  return image;
+}
+
+function ninjaBabySelector()
+{
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector()
+{
+  var selectDiv = $('.pics');
+  return selectDiv;
+}
+
+function firstListItem()
+{
+  var firstItem = $('ul:first-child pic-list');
+  return firstItem;
+}
