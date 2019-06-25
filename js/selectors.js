c5d638f4f1e0f7f1aaad9a4aad7ead6ea66286fa
Paul Yun <p.yun1994@gmail.com>
2019-06-25 21:46:29 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function lastImageSelector(){
+  return $('#pic-list li:last-child img')
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics')
+}
+
+function firstListItem(){
+  return $('#pic-list li:first-child')
+}
