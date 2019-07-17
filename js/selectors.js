de71b86ee12023f46356d98d1132cfa81b88aca4
effective-role-2041 <steve.alex@hotmail.co.uk>
2019-07-17 15:51:44 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  return $('p');
+}
+
+function lastImageSelector(){
+  return $('img:last-child');
+}
+
+function ninjaBabySelector(){
+  return $('#baby-ninja')
+}
+
+function divSelector(){
+  return $('.pics');
+}
+
+function firstListItem(){
+  
+}
