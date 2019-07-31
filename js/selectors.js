6a65186971648377861c29655826cbfb0e2365a7
Samir Jasarat <samirjasarat@gmail.com>
2019-07-31 21:23:16 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,22 @@
 // declare your functions here...
+
+function paragraphSelector () {
+  return $('p');
+}
+
+function lastImageSelector () {
+  return $('img:last');
+}
+
+//ninjaBabySelector using an id selector to return an image
+function ninjaBabySelector () {
+  return $('#baby-ninja');
+}
+
+function divSelector () {
+  return $('.pics');
+}
+
+function firstListItem () {
+  return $('#div pic-list:first-child');
+}
