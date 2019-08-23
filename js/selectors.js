fb39664521f5ab2c0112e49d0cfd6a226d92d600
Anastasia Pankina <anastaciap@ymail.com>
2019-08-23 17:18:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+$(document).ready(paragraphSelector);
+function paragraphSelector(){
+return $('p');
+}
+$(document).ready(lastImageSelector);
+function lastImageSelector(){
+  return $("div img:last");
+}
+
+$(document).ready(ninjaBabySelector);
+function ninjaBabySelector(){
+  return $("#baby-ninja");
+}
+
+$(document).ready(divSelector);
+function  divSelector(){
+  return $(".pics");
+  
+}
+
+$(document).ready(firstListItem);
+function firstListItem(){
+  return $("div li:first-child");
+}
