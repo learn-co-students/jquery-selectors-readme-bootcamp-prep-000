7fe7a35bbbe0930144e4d2dc06581ea0923796a7
Jayd Robinson <jaydr119@gmail.com>
2019-09-18 12:06:59 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector() {
+  var paragraph = $('p');
+  return paragraph;
+
+}
+
+function lastImageSelector() {
+  var lastImage = $('div img').last();
+  return lastImage;
+}
+
+function ninjaBabySelector() {
+  var ninjaBaby = $('#baby-ninja');
+  return ninjaBaby;
+}
+
+function divSelector() {
+  var pics = $('.pics');
+  return pics;
+}
+
+function firstListItem() {
+  var firstChild = $('ul li:first-child');
+  return firstChild;
+}
