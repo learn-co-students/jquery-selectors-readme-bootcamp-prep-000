5126cc54c7ed448fbfc7547e9fbca37633ddc30f
Jennifer Vargas <jevargasv1990@gmail.com>
2019-09-23 22:12:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,19 @@
-// declare your functions here...
+function paragraphSelector() {
+  return $('p');
+}
+
+function lastImageSelector() {
+  return $('.pics img:last');
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja');
+}
+
+function divSelector() {
+  return $('.pics');
+}
+
+function firstListItem() {
+  return $('.pics ul li:first-child');
+}
