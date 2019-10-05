2aa8ee28be69809585051c1c600dac94de171dac
Georgi Petrushev <figoto660@gmail.com>
2019-10-05 05:14:07 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+function paragraphSelector(){
+  var tagP = $("p");
+  return tagP;
+}
+function lastImageSelector() {
+  var imgTag= $('img:last');
+  return imgTag;
+}
+
+
+function ninjaBabySelector() {
+  var babyNinja =  $('#baby-ninja');
+  return babyNinja;
+}
+
+function divSelector() {
+  var pic = $('.pics');
+
+  return pic;
+}
+
+function firstListItem() {
+  var firstList =  $('ul li:first-child');
+  return firstList;
+}
