a1d4c292170216c553754d016a5bea51ed9cd945
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-08 17:17:33 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,32 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  var ptags = [];
+  ptags = $('p');
+  return ptags;
+}
+
+function lastImageSelector(){
+  var img;
+  img = $('img:last');
+  return  img
+}
+
+function ninjaBabySelector(){
+  var img;
+  img = $('#baby-ninja');
+  return img;
+}
+
+function divSelector(){
+  var div = [];
+  div = $('.pics');
+  return div;
+  
+}
+
+function firstListItem(){
+  var l;
+  l = $('ul li:first-child');
+  return l;
+}
