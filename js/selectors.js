30faabba62d10e39a7c28fd82aa06edb27252cc7
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-10 18:50:27 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+var paragraphSelector = function(){
+  return $('p')
+}
+
+var lastImageSelector = function(){
+  return $('img:last')
+}
+
+var ninjaBabySelector = function(){
+ return  $('img#baby-ninja')
+}
+
+var divSelector = function(){
+  return $('div.pics')
+}
+
+var firstListItem = function(){
+ return $('ul#pic-list li:first-child') 
+}
