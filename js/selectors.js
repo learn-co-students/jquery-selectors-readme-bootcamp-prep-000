d2c95232d5bf8dd7b7ec2c4f5290399db9c2589a
masterful-array-3850 <crgammage@gmail.com>
2020-04-12 17:15:52 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,15 @@
-// declare your functions here...
+function paragraphSelector() {
+  document.getElementsByTagName('p')
+}
+function lastImageSelector() {
+  $('img:last')
+}
+function ninjaBabySelector() {
+  $('#baby-ninja')
+}
+function divSelector() {
+  $(.pics)
+}
+function firstListItem() {
+  $('#pic-list ul:first-child')
+}
