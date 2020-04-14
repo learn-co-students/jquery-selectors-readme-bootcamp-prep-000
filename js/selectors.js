56a45df810b01b769881f5d86f94f76c8c0305ee
Jake G <jake098877@gmail.com>
2020-04-14 17:23:21 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
-// declare your functions here...
+function paragraphSelector() {
+  $('p')
+  return $('p')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('div.pics')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}// declare your functions here...
