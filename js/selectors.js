2d5ff763b255fdfe696778fc7fe25ed86a9259d1
Mark Semotiuk <marksem@gmail.com>
2020-04-27 19:20:32 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
-// declare your functions here...
+function paragraphSelector(){
+  output = $('p')
+  return output
+}
+
+function lastImageSelector(){
+  output = $('img:last')
+  return output
+}
+
+function ninjaBabySelector(){
+  output = $('#baby-ninja')
+  return output
+}
+
+function divSelector(){
+  output = $('.pics')
+  return output
+}
+
+function firstListItem(){
+  output = $('#pic-list li:first-child')
+  return output
+}
