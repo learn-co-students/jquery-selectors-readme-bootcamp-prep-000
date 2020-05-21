c38050e658d6cc2667773ce778b096020ad142bd
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-21 20:11:30 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,31 @@
 // declare your functions here...
+
+function paragraphSelector(){
+  
+  return $("p");
+  
+}
+
+function lastImageSelector(){
+  
+  return $("img:last"); 
+   
+}
+ 
+function ninjaBabySelector(){
+  
+  return $("#baby-ninja");
+  
+}
+
+function divSelector(){
+  
+  return $(".pics");
+  
+}
+
+function firstListItem(){
+  
+  return $("ul li:eq(0)");
+  
+}
