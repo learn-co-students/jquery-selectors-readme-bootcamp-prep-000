0dcfcce75ce42361c2e04997a4961c6c97a02e17
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-05 17:46:19 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,20 @@
 // declare your functions here...
+function paragraphSelector(){
+  return $('p')
+}
+
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+
+function divSelector() {
+  return $('.pics')
+}
+
+function lastImageSelector() {
+  return $('img:last')
+}
+
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
