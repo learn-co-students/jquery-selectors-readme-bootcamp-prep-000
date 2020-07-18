6cf1e49084629a7480a3748cec19a0d142387c5d
algebraic-acrobat-1174 <mayowaa4@gmail.com>
2020-07-18 01:05:45 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+function paragraphSelector(){
+  var elements = $('p')
+  return elements
+}
+
+function lastImageSelector(){
+  var image = $('img:last')
+  return image
+}
+
+function ninjaBabySelector(){
+  var baby = $('#baby-ninja')
+  return baby
+}
+
+function divSelector(){
+return $('.pics')
+}
+
+function firstListItem(){
+  var item = $('ul li:first-child')
+  return item
+}
