ed8539b45211661cb1da78d49bf62d0a2e061abc
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-26 18:57:11 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,25 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p');
+  return $('p');
+}
+
+function lastImageSelector(){
+  $('img:last');
+  return $('img:last');
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja');
+  return $('#baby-ninja');
+}
+
+function divSelector(){
+  $('div.pics');
+  return $('div.pics');
+}
+
+function firstListItem(){
+  $('ul li:first-child');
+  return $('ul li:first-child');
+}
